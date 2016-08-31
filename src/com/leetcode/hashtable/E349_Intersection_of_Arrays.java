cbcc140323f8608ed5031e0c6fbaa615865d8a20
Michael Yuan <mtyylx@gmail.com>
2016-08-31 11:08:40 +0800

    Add 3rd E349 solution.

--- src/com/leetcode/hashtable/E349_Intersection_of_Arrays.java
+++ src/com/leetcode/hashtable/E349_Intersection_of_Arrays.java
@@ -22,12 +22,41 @@ public class E349_Intersection_of_Arrays {
     public static void main(String[] args) {
         int[] a = {1, 4, 7, 29, 30, 59, 67};
         int[] b = {2, 5, 6, 30, 59, 55};
-        int[] result = intersection(a, b);
+        int[] result = intersection3(a, b);
     }
 
-    // 先把两个数组都排序后用双指针扫描解法，o(nlogn)
+    // 二分查找，两数组
     // 当然也可以只让一个数组有序，然后遍历另一个数组的每个元素，边遍历边与已排序数组进行二分查找比对
     // 所以计算量依然是n次的logn二分查找。
+    static int[] intersection3(int[] a, int[] b) {
+        int[] small = a.length < b.length ? a : b;
+        int[] big = a.length >= b.length ? a : b;
+        Arrays.sort(big);
+        Set<Integer> set = new HashSet<>();
+        int i, j, mid;
+        for (int x = 0; x < small.length; x++) {
+            // 每扫描完一个元素，必须重置首尾指针哦
+            i = 0;
+            j = big.length - 1;
+            while (i <= j) {
+                mid = (i + j) / 2;
+                if      (big[mid] < small[x]) i = mid + 1;
+                else if (big[mid] > small[x]) j = mid - 1;
+                else    {
+                    set.add(small[x]);
+                    break;      // 找到后得立刻退出循环
+                }
+            }
+        }
+        int[] result = new int[set.size()];
+        int idx = 0;
+        for (int ele : set) {
+            result[idx++] = ele;
+        }
+        return result;
+    }
+
+    // 先把两个数组都排序后用双指针扫描解法，因为有排序的复杂度在那，所以是o(nlogn)
     static int[] intersection2(int[] a, int[] b) {
         Arrays.sort(a);
         Arrays.sort(b);
