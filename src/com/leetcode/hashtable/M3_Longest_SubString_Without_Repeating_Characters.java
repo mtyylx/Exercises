7d203c95c4788640b20b9329ca5df24a50e0edf7
Michael Yuan <mtyylx@gmail.com>
2016-09-05 23:10:03 +0800

    Comments on M3 solution.

--- src/com/leetcode/hashtable/M3_Longest_SubString_Without_Repeating_Characters.java
+++ src/com/leetcode/hashtable/M3_Longest_SubString_Without_Repeating_Characters.java
@@ -19,23 +19,32 @@ import java.util.Map;
  * */
 public class M3_Longest_SubString_Without_Repeating_Characters {
     public static void main(String[] args) {
-        System.out.println(longestSubStringWithoutRepeat("pwwkew"));
+        System.out.println(longestSubStringWithoutRepeat2("abbabbabcdadckdiac"));
     }
 
+    // 哈希表+双指针（快慢指针）解法，
+    // 基本思路：
+    // 维护一个哈希表，从第一个元素开始将元素值作为键，元素索引作为值存入哈希表
+    // j为快指针，不断添加或更新现有元素最近一次出现的索引位置
+    // i仅在遇到重复元素时，才会更新为重复的元素最近一次出现的位置的下一个元素，作为重新计算长度的起始位置
+    // 每当快指针移动一次，都重新计算当前已经达到的最大长度
     static int longestSubStringWithoutRepeat2(String a) {
         if (a == null || a.length() == 0) return 0;
         int result = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (int i=0, j=0; i<a.length(); ++i){
-            if (map.containsKey(a.charAt(i))){
-                j = Math.max(j, map.get(a.charAt(i))+1);
+        for (int i = 0, j = 0; j < a.length(); j++){
+            if (map.containsKey(a.charAt(j))){
+                // 取max是为了避免abba这种情况，即b重复完了以后又出现了a重复，如果但这时候不能从a的上次出现的接下来位置，而是应该从i的位置开始重新计算
+                i = Math.max(i, map.get(a.charAt(j)) + 1);
             }
-            map.put(a.charAt(i), i);
-            result = Math.max(result, i - j + 1);
+            map.put(a.charAt(j), j);
+            result = Math.max(result, j - i + 1);
         }
         return result;
     }
 
+    // 这个解法思路其实和上面是相似的，但是区别在于遇到重复元素以后怎么处理。
+    // 这里选择把整个哈希表都清空，然后指针位置从上次出现重复元素的位置开始，重新扫描添加，所以会效率很低。
     static int longestSubStringWithoutRepeat(String a) {
         if (a == null || a.length() == 0) return 0;
         int i = 0;
