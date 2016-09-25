28e523120cceec6ac94276f4e4d86da149e758f4
Michael Yuan <mtyylx@gmail.com>
2016-09-25 23:55:02 +0800

    Add M5.

---
+++ src/com/leetcode/string/M5_Longest_Palindrome_Substring.java
@@ -0,0 +1,21 @@
+package com.leetcode.string;
+
+/**
+ * Created by Michael on 2016/9/25.
+ * Given a string S, find the longest palindromic substring in S.
+ * You may assume that the maximum length of S is 1000,
+ * and there exists <one unique> longest palindromic substring.
+ *
+ * Function Signature:
+ * public static String longestPalindromeSubString(String a) {...}
+ * */
+public class M5_Longest_Palindrome_Substring {
+    public static void main(String[] args) {
+
+    }
+
+    // 首先想到的肯定是双层for循环扫描双指针，time - o(n^2)
+    static String longestPalindromeSubstring(String a) {
+
+    }
+}
