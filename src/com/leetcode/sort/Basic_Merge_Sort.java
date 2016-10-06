9e510ff2411f5bbb9d8b41c420354b395fc4531c
Michael Yuan <mtyylx@gmail.com>
2016-10-06 22:32:29 +0800

    Provide more insight into In-place Merge Sort.

--- src/com/leetcode/sort/Basic_Merge_Sort.java
+++ src/com/leetcode/sort/Basic_Merge_Sort.java
@@ -22,31 +22,50 @@ public class Basic_Merge_Sort {
     }
 
     /** 迭代解法 - 直接合并 */
-    // 直接合并 - BottomUp Iterative：相当于省略了递归方法不断除二分解将数组分段的过程，
-    // 直接指定最小比较区段就是1，1比完了比2，2比完了比3，一直比到数组长度N
-    // 外循环控制区段长度，内循环用区段长度扫描并按区段长度merge整个数组
-    // 难点: 在于如何处理长度不是2的幂的数组的merge
-    // 将i设计为merge两个区段各自的长度，而不是总长度，
-    // 然后将j跳跃的间隔设计为2倍的i，可以确保mid的值不会越界，再用Math.min确保right的值也不越界
-    // len = 9:
-    // 0 1 | 2 3 | 4 5 | 6 7 | 8
-    // 0 1 2 3 | 4 5 6 7 | 8
-    // 0 1 2 3 4 5 6 7 | 8
-    // len = 6:
-    // 0 1 | 2 3 | 4 5
-    // 0 1 2 3 | 4 5
+    // Bottom-Up Iterative：相当于省略了递归方法不断除二分解将数组分段的过程，
+    // 直接指定最小比较区段就是1，1比完了比2，2比完了比4，一直比到数组长度N
+    /** 循环指针含义 */
+    // 记住内外循环的目的是为了构造正确的left, mid, right值给merge方法使用，而不是直接当场扫描并合并
+    // 外循环i表示每次划分的<左右分区各自宽度>，应为1, 2, 4, 8, 16...，因此有：i *= 2
+    // 内循环j表示每次划分的<左分区起始位置>，j自增间隔等于两个i（即左右区间长度之和），因此有：j += 2 * i
+    // 在i=1时应为0, 2, 4, 6, 8...
+    // 在i=2时应为0, 4, 8, 12, 16...
+    // 在i=3时应为0, 8, 16, 24, 32...
+    // 在i=4时应为0, 16, 32, 48, 64...
+    /** merge调用方式 */
+    // left = j; 因为j就表示做分区起始位置
+    // mid = j + i - 1; 因为起始位置j加上分区宽度i就是中点，减一就是左分区的终止位置
+    // right = j + 2 * i - 1; 因为起始位置加上左右分区的宽度2*i就是总长度，减一就是右分区的终止位置
+    // 还要考虑到对于长度不是2的幂的数组，需要避免mid和right越界，因此在使用前需要和a.length-1进行比较，取小的那个。
+    /** 举例 */
+    // {9, 8, 7, 4, 5}
+    // Merge(0, 0, 1): 9 8 -> 8 9
+    // Merge(2, 2, 3): 7 4 -> 4 7
+    // Merge(4, 4, 4): 5, do nothing. 如果没有Math.min保护，原本应是(4, 4, 5), right会越界。
+    // Merge(0, 1, 3): 8 9 4 7 -> 4 7 8 9
+    // Merge(4, 4, 4): 5, do nothing. 如果没有Math.min保护，原本应是(4, 5, 7), mid和right都会越界。
+    // Merge(0, 3, 4): 4 7 8 9 5 -> 4 5 7 8 9
+    // {4, 5, 7, 8, 9}
     static void MergeSort_Iterative(int[] a) {
-        for (int i = 1; i < a.length; i *= 2)
-            for (int j = 0; j < a.length - i; j += 2 * i)
-                merge3(a, j, j + i - 1, Math.min(j + 2 * i - 1, a.length - 1));
+        for (int i = 1; i < a.length; i *= 2) {
+            for (int j = 0; j < a.length; j += 2 * i) {
+                merge3(a, j, Math.min(j + i - 1, a.length - 1), Math.min(j + 2 * i - 1, a.length - 1));
+                // System.out.println("Merge(" + j + ", " + Math.min(j + i - 1, a.length - 1) + ", " + Math.min(j + 2 * i - 1, a.length - 1) + ")");
+            }
+        }
     }
 
     /** 递归解法 - 先分解再合并 */
+    // Top-down Recursive: 逆序递归，即先递归至终点，再在返回的过程中进行Conquer
+    // 使用ArrayList实现merge功能，双指针同时扫描两段，并存入ArrayList，最后由ArrayList覆盖当前区间
+    // 用极大值保护先扫描入库完成的那个部分，直至左右两部分都扫完。
+    // 算法导论里介绍的方法是先把左右两段元素备份至一个临时数组，然后用双指针扫描更新原数组，
+    // 下面的解法则是直接双指针扫描数组两段，存入临时数组，然后顺序扫描更新现有数组，本质上是一样的，不过感觉我的方法写起来更简单些
+    // 需要注意的一点是，这里的left / mid / right索引都是inclusive的，因此在调用的时候应该调用MergeSort(a, 0, a.length - 1)
     static void MergeSort_Recursive(int[] a) {
         MergeSortRecursive(a, 0, a.length - 1);
     }
 
-    // 先分解再合并 - Topdown Recursive: 逆序递归，即先递归至终点，再在返回的过程中进行Conquer
     static void MergeSortRecursive(int[] a, int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2;
@@ -56,11 +75,6 @@ public class Basic_Merge_Sort {
         }
     }
 
-    // 使用ArrayList实现merge功能，双指针同时扫描两段，并存入ArrayList，最后由ArrayList覆盖当前区间
-    // 用极大值保护先扫描入库完成的那个部分，直至左右两部分都扫完。
-    // 算法导论里介绍的方法是先把左右两段元素备份至一个临时数组，然后用双指针扫描更新原数组，
-    // 下面的解法则是直接双指针扫描数组两段，存入临时数组，然后顺序扫描更新现有数组，本质上是一样的，不过感觉我的方法写起来更简单些
-    // 需要注意的一点是，这里的left / mid / right索引都是inclusive的，因此在调用的时候应该调用MergeSort(a, 0, a.length - 1)
     static void merge(int[] a, int left, int mid, int right) {
         List<Integer> list = new ArrayList<>(right - left + 1);
         int i = left;
@@ -135,7 +149,7 @@ public class Basic_Merge_Sort {
             for (int i = 1; i < 100; i++) {
                 int[] x = randGen(i, j);
                 System.out.println("Original: " + Arrays.toString(x));
-                MergeSort_Iterative(x);
+                MergeSort_Recursive(x);
                 if (!isSorted(x)) {
                     System.out.println("Failed at: " + Arrays.toString(x));
                     return;
