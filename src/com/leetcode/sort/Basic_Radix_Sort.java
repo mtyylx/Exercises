e200ea9a3db555a31de0ba41bb2a52947af58fa4
Michael Yuan <mtyylx@gmail.com>
2016-10-07 12:24:51 +0800

    Comments on the prerequisite for stable sub-sorting algorithm of Radix Sort.

--- src/com/leetcode/sort/Basic_Radix_Sort.java
+++ src/com/leetcode/sort/Basic_Radix_Sort.java
@@ -10,15 +10,37 @@ import java.util.Arrays;
  */
 public class Basic_Radix_Sort {
     public static void main(String[] args) {
-        int[] a = {9, 6, 4, 6, 1, 6};
-        RadixSort(a);
+        int[] a = {18, 16};
+        RadixSort2(a);
         System.out.println(Arrays.toString(a));
         bulkTest();
     }
 
     // Radix Sort的核心在于对数值类型元素的每一位进行排序。
-    // Radix Sort中对每一位进行排序的子排序算法必须是稳定的，因为每一位在排序时其实都携带了该元素的其他位作为卫星数据。
     // 需要从低位向高位扫描，且需要知道数组元素最大位数，作为循环条件。
+    // Radix Sort中对每一位进行排序的子排序算法必须是稳定的，因为每一位在排序时其实都携带了该元素的其他位作为卫星数据。
+    // 子排序算法必须稳定的示例：
+    // 对 {18, 16} 进行排序，
+    // 在对个位排序后变为 {16, 18}
+    // 在对十位排序后，由于十位是一样的，因此应该以个位的排序为准，
+    // 但是如果算法是不稳定的，比如计数排序如果没有使用逆序填充，则会得到 {18, 16}，导致排序失败。
+
+    /** 使用Counting Sort作为子排序方法 */
+    static void RadixSort2(int[] a) {
+        int max = 0;
+        for (int x : a) max = Math.max(getLength(x), max);
+
+        for (int c = 0; c < max; c++) {
+            int[] b = new int[10];
+            int[] d = new int[a.length];
+            for (int x : a) b[getDigit(x, c)]++;
+            for (int i = 1; i < 10; i++) b[i] += b[i - 1];
+            for (int i = a.length - 1; i >= 0; i--) d[--b[getDigit(a[i], c)]] = a[i];   // 必须逆序扫描以确保稳定性。
+            for (int i = 0; i < a.length; i++) a[i] = d[i];
+        }
+    }
+
+    /** 使用Insertion Sort作为子排序方法 */
     static void RadixSort(int[] a) {
         int max = 0;
         for (int x : a) max = Math.max(getLength(x), max);
@@ -52,11 +74,11 @@ public class Basic_Radix_Sort {
     }
 
     public static void bulkTest() {
-        for (int j = 2; j < 1000; j += 2) {
-            for (int i = 1; i < 100; i++) {
+        for (int j = 2; j < 20; j += 2) {
+            for (int i = 1; i < 10; i++) {
                 int[] x = randGen(i, j);
                 System.out.println("Original: " + Arrays.toString(x));
-                RadixSort(x);
+                RadixSort2(x);
                 if (!isSorted(x)) {
                     System.out.println("Failed at: " + Arrays.toString(x));
                     return;
