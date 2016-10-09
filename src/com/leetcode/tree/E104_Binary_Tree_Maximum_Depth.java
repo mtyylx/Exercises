0c378ec3c0190f48f187571a9c9ad7bb20589988
Michael Yuan <mtyylx@gmail.com>
2016-10-10 01:23:46 +0800

    Add a new DFS + Stack iterative solution.

--- src/com/leetcode/tree/E104_Binary_Tree_Maximum_Depth.java
+++ src/com/leetcode/tree/E104_Binary_Tree_Maximum_Depth.java
@@ -14,7 +14,7 @@ import java.util.Deque;
 public class E104_Binary_Tree_Maximum_Depth {
     public static void main(String[] args) {
         TreeNode root = TreeNode.Generator(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
-        System.out.println(depth(root));
+        System.out.println(depth2(root));
     }
 
     // DFS + Stack: Recursive
@@ -22,9 +22,36 @@ public class E104_Binary_Tree_Maximum_Depth {
         return (root == null) ? 0 : 1 + Math.max(depth(root.left), depth(root.right));
     }
 
-    // BFS + Queue: Iterative.
-    // 跟广度优先的Level Traversal解法一样，只是不用记录节点而已。但是解法看上去很臃肿。
+    // DFS + Stack: Iterative
+    // 使用两个栈，一个栈存节点，一个栈存这个节点对应的高度，
+    // 然后确保两个栈永远同时入栈和出栈，就可以做到保持节点和其高度一直一一对应
+    // 实际上这个过程就等效于手动实现了DFS递归写法函数栈的功能。
     static int depth2(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Deque<Integer> depth = new ArrayDeque<>();
+        if (root == null) return 0;
+        stack.push(root);
+        depth.push(1);
+        int max = 0;
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();     // 取节点
+            int level = depth.pop();            // 取节点的高度
+            max = Math.max(max, level);         // 更新全局最大值
+            if (current.right != null) {
+                stack.push(current.right);      // 记录右节点
+                depth.push(level + 1);          // 同时记录其高度
+            }
+            if (current.left != null) {
+                stack.push(current.left);       // 记录左节点
+                depth.push(level + 1);          // 同时记录其高度
+            }
+        }
+        return max;
+    }
+
+    // BFS + Queue: Iterative.
+    // 跟广度优先的Level Traversal解法一样，只是不用记录节点而已。比DFS的迭代方法效率要高
+    static int depth3(TreeNode root) {
         Deque<TreeNode> queue = new ArrayDeque<>();
         int level = 0;
         if (root == null) return level;
