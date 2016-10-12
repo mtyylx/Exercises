e07806c03909209ff45b3f06e8ada1a584f1d31b
mtyylx <mtyylx@gmail.com>
2016-10-12 14:39:46 +0800

    Add a new iterative solution.

--- src/com/leetcode/tree/E110_Balanced_Binary_Tree.java
+++ src/com/leetcode/tree/E110_Balanced_Binary_Tree.java
@@ -1,7 +1,6 @@
 package com.leetcode.tree;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 /**
  * Created by LYuan on 2016/10/11.
@@ -15,7 +14,7 @@ import java.util.Deque;
 public class E110_Balanced_Binary_Tree {
     public static void main(String[] args) {
         TreeNode root = TreeNode.Generator(new int[] {1, 2, 3, 4, 5, 6, 7});
-        System.out.println(isBalanced(root));
+        System.out.println(isBalanced4(root));
     }
 
     /** DFS + Stack: 递归解法，逆序递归Bottom-Up（需要按照自底向上的顺序传递有用信息：当前子树高度）*/
@@ -62,10 +61,45 @@ public class E110_Balanced_Binary_Tree {
     }
 
     /** 由于任何二叉树的问题都或多或少的需要遍历这个二叉树，也就转变成为了采用某种二叉树遍历方式并顺便在遍历的过程中做一些事情的问题了 */
-    // 这道题由于需要先检查子节点是否是平衡的，再检查父节点，因此特别符合后序遍历算法的顺序，所以可以以这个思路用后序遍历的迭代解法实现。
-    // 其实解法1本质上就是后序遍历。
-    // DFS + Stack: 迭代
-    static boolean isBalanced3(TreeNode root) {
+    // 这道题由于需要先检查子节点是否是平衡的，再检查父节点，因此特别符合后序遍历算法的顺序，所以可以用后序遍历的迭代解法实现。
+    // 另：其实解法1本质上就是后序遍历的递归实现。
 
+    /** 迭代写法：使用DFS + Stack + HashMap: 使用HashMap把节点和节点所在高度一一对应了起来 */
+    // 逻辑分析：
+    // 每个循环开始首先观察栈顶节点，可以看到进入if的前提可以是这几种：
+    // 1. 根本没有左右节点
+    // 2. 只有一个子节点，且该子节点已经出现在哈希表中（已访问）
+    // 3. 两个子节点都有，且都已经出现在哈希表中
+    // 所以进入else的前提就是这几种：
+    // 1. 只有一个子节点，且该子节点没有访问过
+    // 2. 两个子节点都有，且至少有一个没有访问过
+    // 于是就进入else详细检查，如果有左节点且左节点未出现在哈希表中，就把左节点压入，等待下轮循环访问。
+    // 如果没有左节点（但一定有右节点）就把右节点压入，如果有左节点但左节点已经访问过，也要把右节点压入以供访问。
+    // 进入下一循环。
+    // 只要进入了大if，我们就可以确保current的子节点要不然不存在（高度0）要不然存在且已经记录高度，然后判定current这个数是否是平衡的
+    // 如果是平衡的，就把current节点也记录在哈希表中，同时把current所在高度也记录进去。
+    static boolean isBalanced4(TreeNode root) {
+        if (root == null) return true;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Map<TreeNode, Integer> map = new HashMap<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.peek();
+            if (current.left == null && current.right == null ||
+                current.left == null && map.containsKey(current.right) ||
+                current.right == null && map.containsKey(current.left) ||
+                map.containsKey(current.left) && map.containsKey(current.right)) {
+                int left = (current.left == null) ? 0 : map.get(current.left);
+                int right = (current.right == null) ? 0 : map.get(current.right);
+                if (Math.abs(left - right) > 1) return false;
+                else map.put(current, 1 + Math.max(left, right));
+                stack.pop();
+            }
+            else {
+                if (current.left != null && !map.containsKey(current.left)) stack.push(current.left);
+                else stack.push(current.right);
+            }
+        }
+        return true;
     }
 }
