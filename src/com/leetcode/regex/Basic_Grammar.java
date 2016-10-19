20a215ea2f0de66d0837eb59da5792195ae3dcf1
mtyylx <mtyylx@gmail.com>
2016-10-19 18:02:14 +0800

    Add Metacharacters usage.

--- src/com/leetcode/regex/Basic_Grammar.java
+++ src/com/leetcode/regex/Basic_Grammar.java
@@ -9,16 +9,16 @@ import java.util.regex.Pattern;
  */
 public class Basic_Grammar {
     public static void main(String[] args) {
-        quantifier_Test();
+        metacharacter_Test();
     }
 
     /** Selection：选择符 */
-    public static void or_Test() {
+    static void or_Test() {
         find("F(u|i)ck", "Fack Fuck Fick");     // 等效于或运算。
     }
 
     /** Quantification: 数量限定符，限定前面出现的单独这个字符<允许出现的次数>。只有出现次数在规定范围内才算匹配。*/
-    public static void quantifier_Test() {
+    static void quantifier_Test() {
         /** Greedy 贪婪模式（默认）：<尽可能多>的匹配 */
         // 不加: 出现次数 == 1 (1)  如果字符后面没有跟数量限定符，那么就说明这个字符有且仅能出现1次。
         // *: 出现次数 >= 0 (0, 1, n)
@@ -44,7 +44,7 @@ public class Basic_Grammar {
         // ??: 0, 1，尽可能少的匹配
         // {n,}?: >= n，尽可能少的匹配
         // {n, m}?: m>=x>=n，尽可能少的匹配
-        find("a.*b", "abaab");          // 贪婪，从整个字符串开始匹配，不断缩小，因此匹配结果是：abaab at (0, 5)
+        find("a.*b", "abaab");          // 贪婪，在扫描过程中寻找到最大匹配，因此匹配结果是：abaab at (0, 5)
         find("a.*?b", "abaab");         // 懒惰，从字符串开始扫描，只要达到匹配最低标准就算匹配，因此匹配结果有两个：ab 和 aab (没有abaab)
         find("F*", "AFF");              // 贪婪，因此只会返回一个匹配结果：AFF at (0, 4)
         find("F*?", "AFF");             // 懒惰，因为*?最少可以匹配0次，因此匹配结果是：5个空字符串"". 位置分别在(0,0)(1,1)(2,2)(3,3)
@@ -53,14 +53,14 @@ public class Basic_Grammar {
     }
 
     /** 括号：定义操作符的范围和优先顺序 */
-    public static void grouping_Test() {
+    static void grouping_Test() {
         find("gr(a|e)y (gray|grey)", "grey gray");      // 或运算被限制于括号内。
         find("(Shit)*X", "ShitShitX");                  // 括号内作为一个字符看待，*作用于括号内的这个整体。
     }
 
-    public static void metacharacter_Test() {
+    static void metacharacter_Test() {
         /** Meta-Character：元字符，即正则表达式专用的转义字符，用来匹配特定的字符 */
-        // 注意：区别java自己的转义字符\n,\t，以及正则表达式的转义字符\w,\b。后者为了放在字符串中，必须再加一个转义符\，形如"\\w", "\\b"
+        // 注意：区别java自己的转义字符（\n,\t...）和正则表达式专用的转义字符（\w,\b...）。后者放在字符串中必须再加一个转义符\，形如"\\w", "\\b".
         // 点号: 代表任意字符，除了换行符"\n"
         // \w: 代表字母、数字、下划线、汉字
         // \s: 代表空白符（空格，中文全角空格，换行"\n"，制表符"\t"）
@@ -68,15 +68,41 @@ public class Basic_Grammar {
         // \b: 代表位置 -> 单词的开始和结束
         // ^: 代表位置 -> 整个字符串的开始
         // $: 代表位置 -> 整个字符串的结束
-        print(".*", "a;lsdkfj");                            // .*表示匹配任意长度的任意字符串，只要这个字符串内没有换行符就匹配。
+
+        /** < . > 匹配内容：任意字符 */
+        // .*表示任意长度的任意字符串，只要遇到换行符就算匹配结束。
+        // 因此下面语句返回匹配结果如下：
+        // => "abc" at (0, 3)
+        // => "" at (3, 3)
+        // => "12345 at (4, 9)
+        // => "" at (9, 9)
+        // => "" at (10, 10)
+        // 为什么有这么多空字符串？让我们来分析一下。首先分解原字符串：
+        //    a   b   c   \n   1   2   3   4   5   \n
+        //  ↑   ↑   ↑   ↑    ↑   ↑   ↑   ↑   ↑   ↑    ↑
+        //  0   1   2   3    4   5   6   7   8   9    10
+        // 从左向右扫描，寻找最大匹配（即只有遇到换行符才结束匹配）
+        // 匹配1: 0 - 3 = "abc"
+        // 匹配2: 3 - 3 = ""
+        // 匹配3: 4 - 9 = "12345"
+        // 匹配4: 9 - 9 = ""
+        // 匹配5: 10 - 10 = "" (容易忽视，最后一个字符结尾处也算一个索引。)
+        find(".*", "abc\n12345\n");
+
+        /** < \w > 匹配内容：任意非空白字符 */
         print("\\w \\w \\w", "1 c  ");                      // 表示专门匹配三个非空白符的字符，中间用空格相连。例如"1 2 3"
+
+        /** < \s > 匹配内容：任意空白字符 */
         print("\\s*Shit", "    \t   Shit");                 // \s*表示任意多个空白符。
+
+        /** < \d > 匹配内容：任意数字字符 */
         print("Mobile: \\d{11}", "Mobile: 13901234567");    // \d{11}表示匹配十一位连续出现的数字，即手机号。
+
+        /** < \b > 匹配位置：任意单词的开始和结尾位置 */
         print("\\bFuck\\b", "Fuckish");                     // 只匹配Fuck这个单词，不匹配
-        find("(Shit)*", "ShitShitShit");
     }
 
-    public static void stringMatchesAPI_Test() {
+    static void stringMatchesAPI_Test() {
         // Or
         print("F(u|i)ck", "Fack");
 
@@ -112,7 +138,12 @@ public class Basic_Grammar {
     /** 使用 java.util.regex 类库：可以判断正则表达式是否能够匹配所给字符串的局部或全部，如果可以匹配，则给出所有匹配的位置区间 */
     // regex类库是专门进行正则匹配的库，功能很全面和强大。
     // 需要使用Pattern的静态工厂方法构造正则表达式的匹配器(pattern)，
-    // 然后再由待匹配字符串定义一个matcher对象，最后使用find()来搜索。
+    // 然后再由待匹配字符串定义一个matcher对象，使用find()来搜索。
+    // 最后用start() / end()显示匹配的起始位置和终止位置，
+    // 要注意的是，这里的位置索引与数组的索引不同，如下图所示：
+    // 数组索引：    [____0____] [____1____] [____2____] [____3____] [____4____]
+    // 匹配索引：   <0>        <1>         <2>         <3>         <4>        <5>
+    // 所以即使字符串索引只有0-4，匹配索引取值范围也可以达到0-5.
     private static void find(String regex, String str) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
