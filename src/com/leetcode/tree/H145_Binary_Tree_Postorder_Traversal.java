2cf6be2a6ad938b681e1f673c786669cd220d7da
mtyylx <mtyylx@gmail.com>
2016-12-05 16:34:44 +0800

    Update Post-Order Traversal with more execution analysis.

--- src/com/leetcode/tree/H145_Binary_Tree_Postorder_Traversal.java
+++ src/com/leetcode/tree/H145_Binary_Tree_Postorder_Traversal.java
@@ -19,22 +19,47 @@ import java.util.*;
 public class H145_Binary_Tree_Postorder_Traversal {
     public static void main(String[] args) {
         TreeNode root = TreeNode.Generator(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
-        List<Integer> result = postOrderx2(root);
+        System.out.println(postOrder_recursive(root));
+        System.out.println(postOrder_recursive2(root));
+
+        System.out.println(postOrder_iterative(root));
+        System.out.println(postOrder_iterative2(root));
+        System.out.println(postOrder_iterative3(root));
+
+        System.out.println(postOrder_reversePreOrder(root));
+        System.out.println(postOrder_reversePreOrder2(root));
+        System.out.println(postOrder_reversePreOrder3(root));
     }
 
-    // 递归解法
-    static List<Integer> postOrder0(TreeNode root) {
+    /** 递归解法1：每个递归都定义一个result容器 */
+    static List<Integer> postOrder_recursive(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root != null) {
-            result.addAll(postOrder0(root.left));
-            result.addAll(postOrder0(root.right));
+            result.addAll(postOrder_recursive(root.left));
+            result.addAll(postOrder_recursive(root.right));
             result.add(root.val);
         }
         return result;
     }
 
-    /** 解法1.1：真正使用后序遍历的逻辑 */
-    // 虽然解法234写起来很简单，但因为并没有使用真正意义上的后序顺序遍历节点，因此在实际情况中很有可能不能被使用。
+    /** 递归解法2：将result在递归方法之间传递，而不是每个递归方法都创建自己的最后再合并。 */
+    static List<Integer> postOrder_recursive2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postOrder_recursive2(root, result);
+        return result;
+    }
+
+    static void postOrder_recursive2(TreeNode root, List<Integer> result) {
+        if (root == null) return;
+        postOrder_recursive2(root.left, result);
+        postOrder_recursive2(root.right, result);
+        result.add(root.val);
+    }
+
+
+
+    /** 迭代解法1 */
+    // 真正意义上的后序顺序遍历节点，即left -> right -> root.
     //                     1
     //                   /   \
     //                  2     3
@@ -47,7 +72,7 @@ public class H145_Binary_Tree_Postorder_Traversal {
     // 进入下一个while循环，看栈顶元素（此时是节点2）：由于2有右子树，且右子树并不是prev（4），所以向下移动并压栈 [1 2 5
     // 进入下一个while循环，看栈顶元素（此时是prev的父节点2的右子树5）：由于5没有右子树，因此可以直接导出5，并让prev指向这个被弹出的节点5 [1 2
     // 进入下一个while循环，看栈顶元素（此时又回到了节点2）：由于2有右子树，但是右子树已经访问过了，所以可以直接导出2，并让prev指向这个被弹出的节点2 [1
-    static List<Integer> postOrder(TreeNode root) {
+    static List<Integer> postOrder_iterative(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
         TreeNode current = root;
@@ -73,55 +98,69 @@ public class H145_Binary_Tree_Postorder_Traversal {
         return result;
     }
 
-    /** 解法1.2：基于1.1进行简化，推荐的解法 */
-    // 上面的解法重复的部分是每次要向左下移动的while循环，可以将while循环拆成if...else混合在大的while循环中
-    //                     1              |    [1 2 4 <-    output 4, prev 4
-    //                   /   \            |    [1 2 <-      current = peek.right = 5
-    //                  2     3           |    [1 2 5 <-    output 5, prev 5
-    //                /   \     \         |    [1 2 <-      peek.right == prev output 2, prev 2
-    //               4     5     6        |    [1 <-        current = peek.right = 3
-    //                                    |    [1 3 <-      current = peek.right = 6
-    //                                    |    [1 3 6 <-    output 6, prev = 6
-    //                                    |    [1 3 <-      peek.right == prev output 3, prev 3
-    //                                    |    [1 <-        peek.right == prev output 1, prev 1, stack empty, current is null. Exit.
-    /** 难点1：需要使用额外的prev和peek指针 */
-    // 对比中序遍历的if...else解法可以看到其实代码整体结构都差不多，区别在于定义了两个新的节点指针prev和peek.
-    /** prev的用途 */
-    // 是辨识当前节点的右子树是否已经探索过，如果已经探索过，就可以直接输出当前节点，并将prev更新为指向当前节点。
-    // prev赋给左侧节点的时候不会起作用，因为peek.right不可能访问到左侧节点
-    /** peek的用途 */
-    // 是避免每次while循环一开始都进入if区块向左下移动。不使用peek而是全用current会怎么样：
-    // 当current已经触底，将peek到的栈顶元素赋给current时，如果这个节点没有右节点或者已经访问过右节点进入下一循环时，
-    // 因为current已经不是null了，因此current又会再次向左下移动到底，就死循环了。所以这种情况必须跳过if。
-    // 反过来说就是：if块的功能（向左一直到底）只有在当前节点还未探索过左侧分支时才进行，如果已经探索过左侧分支，那么仅会在向右移动一步后探索左侧分支的时候才再用到。
-    static List<Integer> postOrderx(TreeNode root) {
+    /** 迭代解法2：基于上面简化，推荐解法。 */
+    // 使用current / top / prevTop 三个指针。
+    // current负责持续访问节点的左分支。
+    // top用来判断栈顶元素的右分支是否有必要访问。
+    // prevTop用来判断栈顶元素的右分支是否已经访问过。
+    /** 整体逻辑 */
+    // 如果node存在：则先缓存node，然后去node.left
+    // 如果node不存在：
+    //      如果top.right也不存在：说明top是叶子节点，可以直接访问，并出栈。出栈前缓存top节点为prevTop，以用来判断下一个top和当前top的关系。
+    //      如果top.right存在：
+    //              如果top.right就是prevTop节点，说明目前处于<向上访问>过程，top.right已经访问过，就是刚刚出栈的prevTop。
+    //              如果top.right不是prevTop节点，说明目前处于<向下访问>过程，top.right还没有访问过，让node指向top.right继续访问。
+    //
+    //               1
+    //             /   \
+    //           2       3
+    //          / \     / \
+    //       X1   4     5   X6
+    //           /\     /\
+    //         X2 X3   X4 X5
+    //
+    // node -> 1    stack: [ 1
+    // node -> 2    stack: [ 1 2
+    // node -> X1   top -> 2, 因为 top.right -> 4 && prev -> null,     所以 node = top.right -> 4                        top(2)右子树未访问
+    // node -> 4    stack: [ 1 2 4
+    // node -> X2   top -> 4, 因为 top.right -> X3 == null,            所以 访问top.val, prevTop -> 4, stack: [ 1 2       top(4)没有右子树
+    // node -> X2   top -> 2, 因为 top.right -> 4 && prev -> 4,        所以 访问top.val, prevTop -> 2, stack: [ 1         top(2)右子树已经访问
+    // node -> X2   top -> 1, 因为 top.right -> 3 && prev -> 2,        所以 node = top.right -> 3   , stack: [ 1         top(1)右子树未访问
+    // node -> 3    stack: [ 1 3
+    // node -> 5    stack: [ 1 3 5
+    // node -> X4   top -> 5, 因为 top.right -> X5,                    所以 访问top.val, prevTop -> 5, stack: [ 1 3       top(5)没有右子树
+    // node -> X4   top -> 3, 因为 top.right -> X6,                    所以 访问top.val, prevTop -> 3, stack: [ 1         top(3)没有右子树
+    // node -> X4   top -> 1, 因为 top.right -> 3 && prev -> 3,        所以 访问top.val, prevTop -> 1, stack: [ ]         top(1)右子树已经访问
+    // exit.
+
+    static List<Integer> postOrder_iterative2(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
         TreeNode current = root;
-        TreeNode prev = null;
-        while (current != null || !stack.isEmpty()) {
+        TreeNode prevTop = null;
+        while (current != null || !stack.isEmpty()) {       // 因为没有在一开始压入种子节点，为了进入循环，就需要扩大循环条件。
             if (current != null) {
                 stack.push(current);
                 current = current.left;
             }
             else {
-                TreeNode peek = stack.peek();
-                if (peek.right == null || peek.right == prev) {     // 不更新current，current依然是null，这样不会进入if块
-                    result.add(peek.val);
-                    prev = stack.pop();
+                TreeNode top = stack.peek();                         // 判断栈顶元素状况：
+                if (top.right == null || top.right == prevTop) {     // 如果栈顶元素没有右分支，或右分支已经访问过，就可以将栈顶元素出栈。
+                    result.add(top.val);
+                    prevTop = stack.pop();      // 缓存当前栈顶元素，以便判断扫描方向。
                 }
-                else current = peek.right;                          // 更新current，向右下移动一步，再进入下个循环的if块向左一直到底
+                else current = top.right;                            // 如果栈顶元素有右分支，就访问右分支。
             }
         }
         return result;
     }
 
-    /** 解法1.3：也是真正的后序遍历的迭代写法，但是借助了额外的<HashSet>作为判断是否已访问的工具 */
+    /** 迭代解法3：也是真正的后序遍历的迭代写法，但是借助了额外的<HashSet>作为判断是否已访问的工具 */
     // 由于哈希表最终需要记录所有节点，所以空间复杂度是Space - o(2n)
     // 基本逻辑是：如果当前节点没有子节点，或者所有子节点都访问过，就记录当前节点在哈希表中，并同时输出该元素。
     // 如果当前节点的子节点还没有访问过，那么就得先压栈等待访问。
     // 灵感来源于E110判断是否balanced的迭代解法。
-    static List<Integer> postOrderx2(TreeNode root) {
+    static List<Integer> postOrder_iterative3(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
         Set<TreeNode> set = new HashSet<>();
@@ -147,10 +186,10 @@ public class H145_Binary_Tree_Postorder_Traversal {
         return result;
     }
 
-    /** 解法3.1：前序遍历的变体 + 反转 */
+    /** 解法2.1：前序遍历的变体 + 反转 */
     // 由于后序遍历单凭栈实现起来比较困难，而后续遍历(left -> right -> root)和前序遍历(root -> left ->right)的顺序刚好基本上相反
     // 虽然这种方法很巧妙，但是标准的后续遍历实现依然是主流
-    static List<Integer> postOrder31(TreeNode root) {
+    static List<Integer> postOrder_reversePreOrder(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
         if (root == null) return result;
@@ -165,7 +204,7 @@ public class H145_Binary_Tree_Postorder_Traversal {
         return result;
     }
 
-    /** 解法3.2：前序遍历 + 两个栈 */
+    /** 解法2.2：前序遍历 + 两个栈 */
     // 第一个栈负责前序遍历并把元素导出至第二个栈，再对第二个栈出栈就可以得到后序遍历的顺序了，和前序遍历反转的方法思路类似。
     // 可以看到其实第整个结构就是前序遍历，只不过原来直接add进result的结果先被压入了第二个stack中，最后一步的出栈其实跟Collections.reverse功能一模一样
     //                     1
@@ -175,7 +214,7 @@ public class H145_Binary_Tree_Postorder_Traversal {
     //               4     5     6
     // stack 1: [1 -> [2 3 -> [2 6 -> [2     -> [4 5     -> [
     // stack 2: [  -> [1   -> [1 3 -> [1 3 6 -> [1 3 6 2 -> [1 3 6 2 5 4 -> 出栈：4 5 2 6 3 1
-    static List<Integer> postOrder32(TreeNode root) {
+    static List<Integer> postOrder_reversePreOrder2(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack1 = new ArrayDeque<>();
         Deque<TreeNode> stack2 = new ArrayDeque<>();
@@ -191,10 +230,10 @@ public class H145_Binary_Tree_Postorder_Traversal {
         return result;
     }
 
-    /** 解法3.3：前序遍历 + LinkedList */
+    /** 解法2.3：前序遍历 + LinkedList */
     // 同样使用前序遍历反转，借助于LinkedList用o(1)插入表头可以无需调用reverse或出栈。
     // 这时候就体现出接口的灵活性了，因为接口只要求List，所以内部实现用ArrayList还是LinkedList完全随便用。
-    static List<Integer> postOrder33(TreeNode root) {
+    static List<Integer> postOrder_reversePreOrder3(TreeNode root) {
         List<Integer> result = new LinkedList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
         if (root == null) return result;
