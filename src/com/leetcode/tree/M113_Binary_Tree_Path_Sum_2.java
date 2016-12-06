39a4965b0252d898b9c31f3ae772710236d41fc7
mtyylx <mtyylx@gmail.com>
2016-12-06 10:37:36 +0800

    Clean up the Preorder recursive and Postorder iterative solutions.

--- src/com/leetcode/tree/M113_Binary_Tree_Path_Sum_2.java
+++ src/com/leetcode/tree/M113_Binary_Tree_Path_Sum_2.java
@@ -41,13 +41,24 @@ public class M113_Binary_Tree_Path_Sum_2 {
         List<List<Integer>> result2 = postOrderPathSum(root, 8);
     }
 
-    /** DFS, 递归写法，感觉很像回溯法常用的路径增删法。让我试一下。 */
-    // 相比普通的回溯路径增删法，这里需要小心处理叶子节点（即递归终止条件）原因如下：
+    /** 关于 <实体树结构的问题> 和 <逻辑上具有树结构特征的问题> 在处理上是相同的！ */
+    // “实体树结构”是指要解决的问题给的直接就是树状数据结构对象，每个节点都具有值/左右子树引用。
+    // “逻辑上具有树结构特征”是指要解决的问题在逻辑上是一个树的扩展形态，例如Combination Sum系列中给定candidate数组，每选定一个candidate，就相当与开了一个分支节点。
+    // 一开始我以为这两类问题不能用相同解法解决，现在上面的递归解法证明其实是可以的。
+    // 反而<真正的难点>在于如何用迭代的写法解决这两类问题（实际是同一类问题）：
+    // 因为递归的时候，函数栈的内容可以自动帮你打理好你在每一个节点时所匹配的这些context，例如当前结点下target的剩余值，以及路径状态（走了哪些点）
+    // 但是迭代的时候，就没有人帮你在背后记录这些东西了，你只能靠自己定义的栈来获知当前节点的context，
+    // 因为身在树林中的你是很难获悉自己在这个树中的确切位置的（指在代价很小的情况下获悉，代价很大的话当然就可以做到，但是算法本身效率就低了）
+
+    // 下面是两种解法：Preorder + Recursive (Backtracking) / Postorder + Iterative
+
+    /** 解法1：前序遍历（==回溯法），递归方式
+     *  对于实体树结构的问题，<回溯法>和<前序遍历>是同一件事。 */
+    // 相比于一般的逻辑上具有树结构的回溯路径增删法，这里需要小心处理叶子节点（即递归终止条件）原因如下：
     //      1     target = 1 应该没有解才对。这点要小心。因为1并不算叶子节点，只有抵达真正的叶子节点才能决定是否输出结果。
     //     /
     //    2
-    // 所以递归方法中的原则是：
-    // 一上来就检查当前节点是否为null。
+    // 因此应该一上来就检查当前节点是否为null。
     // 然后把当前结点就加入path中，并对其左右子节点的情况就事论事：
     // 1. 如果当前节点是叶子节点，那么检查target减当前结点值是否为0，是就存储result退出。
     // 2. 如果当前节点不是叶子节点，那么直接递归左右节点（即使没有也没关系，因为下一层递归方法一开始就会退出）
@@ -68,9 +79,7 @@ public class M113_Binary_Tree_Path_Sum_2 {
         current.remove(current.size() - 1);
     }
 
-    /** 关于回溯法与前序遍历法的对比分析：上面的解法是<Backtracking>，下面的解法则是递归写法的标准<Pre-order Traversal> */
-    // 可以看到，两个算法的形式是极为相似的，虽然他们俩解决的具体问题不完全一样，但是思路是完全一样的。
-    // 所以说，其实Backtracking解法使用在实体树问题上就是前序遍历！！！
+    // 上面的解法是<Backtracking>，结构形式与下面的标准前序遍历递归写法<Pre-order Traversal>完全一样。
     // 严格来说这两个算法的着眼点是不同的，回溯强调的是“我可以无限的前进和后退以至穷举所有可能”，而前序遍历强调的是“我要以先root后子节点的方式遍历树”。
     // 但是两者的作用是不约而同的。
     static List<Integer> preOrderTraversal(TreeNode root) {
@@ -86,16 +95,41 @@ public class M113_Binary_Tree_Path_Sum_2 {
         preOrderTraversal(node.right, result);
     }
 
-    /** 关于 <实体树结构的问题> 和 <逻辑上具有树结构特征的问题> 在处理上是相同的！ */
-    // “实体树结构”是指要解决的问题给的直接就是树状数据结构对象，每个节点都具有值/左右子树引用。
-    // “逻辑上具有树结构特征”是指要解决的问题在逻辑上是一个树的扩展形态，例如Combination Sum系列中给定candidate数组，每选定一个candidate，就相当与开了一个分支节点。
-    // 一开始我以为这两类问题不能用相同解法解决，现在上面的递归解法证明其实是可以的。
-    // 反而<真正的难点>在于如何用迭代的写法解决这两类问题（实际是同一类问题）：
-    // 因为递归的时候，函数栈的内容可以自动帮你打理好你在每一个节点时所匹配的这些context，例如当前结点下target的剩余值，以及路径状态（走了哪些点）
-    // 但是迭代的时候，就没有人帮你在背后记录这些东西了，你只能靠自己定义的栈来获知当前节点的context，
-    // 因为身在树林中的你是很难获悉自己在这个树中的确切位置的（指在代价很小的情况下获悉，代价很大的话当然就可以做到，但是算法本身效率就低了）
 
-    /** Post-order Traversal */
+
+    /** 解法2：后序遍历，迭代方式。 */
+    // 在一开始试图用前序遍历的迭代写法解决，但是发现似乎并不能handle节点一次向上跳跃多次的情况。
+    // 后序遍历的迭代解法之所以可以胜任，是因为它的逻辑分支可以区分出什么时候该往path里面添加节点，什么时候该从path里删除节点，完全可以处理跳跃的情况。
+    // 什么时候往path里添加节点：只要current不为null，就先往path里添加。
+    // 什么时候往path里删除节点：栈顶元素没有右分支，或者右分支已经访问过的时候，就可以删除path的尾端。
+    static List<List<Integer>> postOrderPathSum(TreeNode root, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode current = root;
+        TreeNode prevTop = null;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                path.add(current.val);      // 更新路径
+                target -= current.val;      // 更新target
+                current = current.left;
+            }
+            TreeNode top = stack.peek();
+            if (top.right == null || top.right == prevTop) {       // top要不然没有右分支，要不然右分支已经访问过。可以准备回头了。
+                if (top.left == null && top.right == null && target == 0)   // top是叶子节点且target为0，说明找到一条path。
+                    result.add(new ArrayList<>(path));
+                // 不管top有没有左节点，target是不是0，都要回退，target要恢复，path也要回退。
+                target += top.val;
+                path.remove(path.size() - 1);
+                prevTop = stack.pop();                             // 和后序遍历一样，缓存当前栈顶。
+            }
+            else current = top.right;                              // 接着访问top的右分支。
+        }
+        return result;
+    }
+
+    // 上面的解法使用的思路是后序遍历，参照下面标准后续遍历写法，可以看到两者的结构形式完全一样。
     static List<Integer> postOrderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Deque<TreeNode> stack = new ArrayDeque<>();
@@ -118,71 +152,8 @@ public class M113_Binary_Tree_Path_Sum_2 {
         return result;
     }
 
-    /** 同样适用Post-Order Traversal的思路，迭代的解决Path Sum问题。 */
-    static List<List<Integer>> postOrderPathSum(TreeNode root, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode current = root;
-        TreeNode prevTop = null;
-        while (current != null || !stack.isEmpty()) {
-            while (current != null) {
-                stack.push(current);
-                path.add(current.val);      // 更新路径
-                target -= current.val;      // 更新target
-                current = current.left;
-            }
-            TreeNode top = stack.peek();
-            if (top.right == null || top.right == prevTop) {       // top要不然没有右分支，要不然右分支已经访问过。可以准备回头了。
-                if (top.left == null && top.right == null && target == 0)   // top是叶子节点且target为0，说明找到一条path。
-                    result.add(new ArrayList<>(path));
-                // 不管top有没有左节点，target是不是0，都要回退了，因此target要恢复，path也要回退。
-                target += top.val;
-                path.remove(path.size() - 1);
-                prevTop = stack.pop();                             // 和后序遍历一样，缓存当前栈顶。
-            }
-            else current = top.right;                              // 接着访问top的右分支。
-        }
-        return result;
-    }
 
-    /** Case 1 */
-    //       1
-    //      / \
-    //  null   null
-    //    ↑
-    //  current目前位于节点1的左子叶节点（并不存在）。节点1是一个纯粹的叶子节点。
-    //  [应对策略] 将节点1出栈，并访问节点1。
-    //
-    /** Case 2 */
-    //       1
-    //      / \
-    //  null   2
-    //    ↑
-    //  current目前位于节点1的左子叶节点（并不存在）。节点1存在右叶子节点。
-    //  节点1本身不是叶子节点，因此左右子树都必须访问。
-    //  [应对策略] 将节点1出栈，先检查节点1是否有右子树。如果有，就继续访问右子树，如果没有，等效于Case 1. 可以直接Visit节点1了。
-    //
-    /** Case 3 */
-    //       1
-    //      / \
-    //  null   2
-    //         ↑
-    //         current目前位于节点1的右子叶节点（存在）。
-    //  节点2的情况和Case 1完全一样。因此是先压栈2，然后检测到2是纯粹的叶子节点后，就直接把2出栈并访问了。此时缓存了prev就是节点2。
-    //  然后因为此时current一直是null，因此会一直的peek栈顶元素，并检查yua
-
-
-    /** 按理说任何树的问题都应该可以用遍历的四种方式之一来解决。
-     * 这里用Preorder合适，但难点是如何伸缩当前记录的path，使得既保证不同分支间的path是互不影响的，又保证结果和path之间不相互影响。 */
-    // 只要检测到当前的节点是叶子节点，不管匹配不匹配target，都在结束的时候把路径缩短1.
-    // 遍历树的全过程中只使用一个path，path保存的是节点对象
-    // 只有遇到叶子节点的时候，才首先检查是否target达到了0
-    // 然后再peek栈顶的节点（即下一个即将出栈的节点），然后不断的删除path，直至path的最后一个节点是下一个即将出栈节点的父节点。
-
-
-
-    /** DFS + Stack，迭代写法。用了三个栈，存访问节点 + 当前路径结果 + target值（和递归解法根本没区别了，没有意义）*/
+    /** 解法3：DFS + Stack，迭代写法。用了三个栈，存访问节点 + 当前路径结果 + target值（和回溯递归解法根本没区别了，意义不大）*/
     static List<List<Integer>> pathSum2(TreeNode root, int target) {
         List<List<Integer>> result = new ArrayList<>();
         if (root == null) return result;
