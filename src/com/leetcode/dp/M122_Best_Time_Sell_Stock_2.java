f4124be78b4747998d235c037a32dbc0695df6d5
Michael Yuan <mtyylx@gmail.com>
2016-12-10 23:55:16 +0800

    Add M122.

---
+++ src/com/leetcode/dp/M122_Best_Time_Sell_Stock_2.java
@@ -0,0 +1,29 @@
+package com.leetcode.dp;
+
+/**
+ * Created by Michael on 2016/12/10.
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit.
+ * You may complete as many transactions as you like (i.e. buy one and sell one share of the stock multiple times).
+ * However, you may not engage in multiple transactions at the same time (i.e. you must sell the stock before you buy again).
+ *
+ * Function Signature:
+ * public int bestTimeSellStock(int[] a) {...}
+ *
+ * 系列问题：
+ * E121 Best Time Sell Stock 1: 给定股票价格数组，限制交易一次，求最大利润。
+ * M122 Best Time Sell Stock 2: 给定股票价格数组，不限制交易次数，同一时间至多只能持有一笔股票，求最大利润。
+ * H123 Best Time Sell Stock 3: 给定股票价格数组，限制交易次数不大于2次，同一时间至多只能持有一笔股票，求最大利润。
+ * H188 Best Time Sell Stock 4: 给定股票价格数组，限制交易次数不大于K次，同一时间至多只能持有一笔股票，求最大利润。
+ * M309 Best Time Sell Stock 5: 给定股票价格数组，不限制交易次数，同一时间至多只能持有一笔股票，且卖出后需要隔一天才能再买入，求最大利润。
+ *
+ */
+public class M122_Best_Time_Sell_Stock_2 {
+    public static void main(String[] args) {
+
+    }
+
+    static int bestTimeSellStock(int[] a) {
+
+    }
+}
