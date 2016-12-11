d7ccaa71b9773fb8531019c7e55ad02170666dae
Michael Yuan <mtyylx@gmail.com>
2016-12-11 12:42:17 +0800

    Add solution to M122.

--- src/com/leetcode/dp/M122_Best_Time_Sell_Stock_2.java
+++ src/com/leetcode/dp/M122_Best_Time_Sell_Stock_2.java
@@ -20,10 +20,25 @@ package com.leetcode.dp;
  */
 public class M122_Best_Time_Sell_Stock_2 {
     public static void main(String[] args) {
-
+        System.out.println(bestTimeSellStock(new int[] {1, 2, 3, 9, 7, 8}));
     }
 
+    /** 贪心算法：基本思路是<针对每一段上升序列>实时累计最大收益，最后累加收益即可。*/
+    // 思考方式：举例后递推，不断扩大举例规模直至特性显现。
+    // 例1：a = [1, 2, 3] 整体是上升序列，最大收益就是 3(max) - 1(min) = 2，但是[3-1]可以分解为[2-1]+[3-2]以方便实现，相当于是实时更新每天的收益。
+    // 例2：a = [1, 2, 3, 9] 增加了不连续元素9，但是整体仍然是上升序列，最大收益就等于 9(max) - 1(min) = 8.
+    // 例3：a = [1, 2, 3, 9, 7] 增加元素7，只有前半部分是上升序列，但是由于7小于9，因此最大收益仍然就是前半段的最大收益。
+    // 例4：a = [1, 2, 3, 9, 7, 8] 增加元素8，包含两个上升序列，各自的最大收益是8和1，因此总最大收益是9。
+    // 总结起来，可以将 [2 3 5 7 1 4 3 8 9] 分解为三段上升序列 [2 3 5 7] [1 4] [3 8 9]
+    // 然后针对每一段，分解为 7-5+5-3+3-2, 4-1, 9-8+8-3，实时更新累计收益（但并不是在同一天卖出和买入，只是一种等效的分解过程）
     static int bestTimeSellStock(int[] a) {
-
+        if (a == null || a.length == 0) return 0;
+        int profit = 0;
+        int prev = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > prev) profit += a[i] - prev;     // 上升序列时，不断更新收益
+            prev = a[i];
+        }
+        return profit;
     }
 }
