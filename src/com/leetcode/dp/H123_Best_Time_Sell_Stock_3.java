4172bf11f1598276ed727fcbc8e57465a10d3675
mtyylx <mtyylx@gmail.com>
2016-12-13 17:35:04 +0800

    Add 2 more DP solutions to H123.

--- src/com/leetcode/dp/H123_Best_Time_Sell_Stock_3.java
+++ src/com/leetcode/dp/H123_Best_Time_Sell_Stock_3.java
@@ -13,7 +13,7 @@ import java.util.List;
  * Function Signature:
  * public int bestTimeSellStock(int[] a) {...}
  *
- * 系列问题：
+ * 系列问题：（不允许同一天内进行两次操作，先买后卖和先卖后买都不可以）
  * E121 Best Time Sell Stock 1: 给定股票价格数组，限制交易一次，求最大利润。
  * M122 Best Time Sell Stock 2: 给定股票价格数组，不限制交易次数，同一时间至多只能持有一笔股票，求最大利润。
  * H123 Best Time Sell Stock 3: 给定股票价格数组，限制交易次数不大于2次，同一时间至多只能持有一笔股票，求最大利润。
@@ -23,13 +23,46 @@ import java.util.List;
  */
 public class H123_Best_Time_Sell_Stock_3 {
     public static void main(String[] args) {
-        System.out.println(bestTimeSellStock(new int[] {2, 3, 5, 7, 1, 4, 3, 8, 9}));
+        System.out.println(bestTimeSellStock(new int[] {1, 2, 4, 2, 5, 7, 2, 4, 9, 0}));
+        System.out.println(bestTimeSellStock2(new int[] {1, 2, 4, 2, 5, 7, 2, 4, 9, 0}));
+        System.out.println(bestTimeSellStock3(new int[] {1, 2, 4, 2, 5, 7, 2, 4, 9, 0}));
     }
 
     // 错误解法：认为只要按照M122的方法记录下来所有每个分组的profit值然后取最大的两个加起来即可。但是这种策略未能考虑到多个分区合并后的最大值是会变化的。
     // 按照M122的策略，处理这种情况是对的： [2 3 5 7 1 4 3 8 9] 分解为三段上升序列 [2 3 5 7] [1 4] [3 8 9]，profit = 5, 3, 6, 因此最大为5+6=11
     // 但是处理这种情况就不行了：[2 3 5 7 3 9 3 8 9] 分解为 [2 3 5 7] [3 9] [3 8 9], profit = 5, 6, 6, 但最大并不是6+6=12，
     // 而是[2 3 5 7 3 9] [3 8 9] 的7+6=13。
+
+    /** 解法3：通用DP解法，使用二维数组来备忘，解法来自于H188。 */
+    // 这里的最大交易次数是2，因此只需要一个3行的二维数组即可。
+    static int bestTimeSellStock3(int[] a) {
+        int[][] dp = new int[3][a.length];
+        for (int i = 1; i < 3; i++) {
+            int max = -a[0];
+            for (int j = 1; j < a.length; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], a[j] + max);
+                max = Math.max(max, dp[i - 1][j - 1] - a[j]);
+            }
+        }
+        return dp[2][a.length - 1];
+    }
+
+    /** 解法2：DP的变体，省略了备忘用的二维数组，用四个变量代替。更为简洁。 */
+    // 正号表示卖出的收益，负号表示买入时的花销（即负收益）
+    static int bestTimeSellStock2(int[] a) {
+        int buy1 = Integer.MIN_VALUE;
+        int buy2 = Integer.MIN_VALUE;
+        int sell1 = 0;
+        int sell2 = 0;
+        for(int i = 0; i < a.length; i++){
+            sell2 = Math.max(sell2, buy2 + a[i]);   // 选择在第i天卖出，交易收益为buy2 + a[i]，如果能比sell2大就更新sell2
+            buy2 = Math.max(buy2, sell1 - a[i]);    // 选择在第i天买入，交易收益为sell1 - a[i]（第一次交易的总收益加这次花销），如果能比buy2大就更新buy2
+            sell1 = Math.max(sell1, buy1 + a[i]);   // 选择在第i天卖出，交易收益为buy1 + a[i]，如果能比sell1大就更新sell1
+            buy1 = Math.max(buy1, - a[i]);          // 选择在第i天买入，交易收益为-a[i]（因为这是第一次交易），如果能比buy1大就更新buy1
+        }
+        return sell2;
+    }
+
 
     /** 解法1：正反两次扫描解法。Time - o(2n), Space - o(n) */
     // 第一次进行正向扫描：用额外的数组记录从最左端（i = 0）到每一天（i = 1 to len）的区间内可以达到的<单次>最大收益。
@@ -57,6 +90,7 @@ public class H123_Best_Time_Sell_Stock_3 {
     }
 
 
+    // 下面只是我一开始的思路，但后来发现不太有效。不应该以上升序列为主，而应该以交易为主。
     // 起初我的思路找到相邻两个分段之间的最大收益，然后步步为营，但是后来发现每新加一段，就需要检查新的一段和已扫描所有段结合的收益，算法反而复杂化了。
     // 如果上升序列段小于等于2段，那么最大profit一定是两段之和。无需考虑
     // 上升序列的定义：序列中每个元素的值都大于等于左侧相邻元素的值。
