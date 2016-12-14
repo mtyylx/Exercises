c53738ab0d7c8d623f1a8b69e38595ee537e325d
Michael Yuan <mtyylx@gmail.com>
2016-12-15 01:17:01 +0800

    Add part of the amazing Two Pointer solution to M11.

--- src/com/leetcode/array/M11_Container_With_Most_Water.java
+++ src/com/leetcode/array/M11_Container_With_Most_Water.java
@@ -14,9 +14,39 @@ package com.leetcode.array;
 public class M11_Container_With_Most_Water {
     public static void main(String[] args) {
         System.out.println(maxContainer(new int[] {5, 3, 6, 6, 8, 6, 12, 8}));
+        System.out.println(maxContainer2(new int[] {5, 3, 6, 6, 8, 6, 12, 8}));
     }
 
-    /** 解法1：最朴素解法。双指针遍历。Time - o(n^2), Space - o(1) */
+    /** 解法2：双指针首尾相向扫描。优化解空间扫描。Time - o(n) */
+    // 这个问题的难点在于如何让两个指针运动的同时找到一种可靠的变化趋势。
+    // 双指针常见的几种运动方式有：
+    // #1. 快慢指针：即先后平行向同一个方向运动
+    // #2. 中心扩散：即从序列的中间某一点分别向左右运动（排序中常用到）
+    // #3. 首尾包围：即从序列的首尾相向运动直至汇合。
+    // 针对这个问题，make sense的是第三种。
+
+    // 方案#1和#2都是扩张矩形，在这个过程中，不管选择移动哪一边，都无法确定面积会一定增大或一定减小，也就是说，“找不到一种可靠的变化趋势”。
+    // 既然找不到一种趋势，我们也就没有移动A指针而不移动B指针的根据，那么在这种情况下，为了避免错过最优解，我们只能尝试所有可能，即退化成为穷举法。
+    // 方案#3则是收缩矩形，在这个过程中，我们找到了一种可靠的趋势：
+    // 指针A在左，指针B在右，如果指针A所在高度 > 指针B所在高度，那么当前矩形面积由指针B所决定，
+    // 此时如果我们收缩指针A，则不管下一个高度是什么，矩形面积一定会减小。
+    // * 如果下一个高度增加，但矩形高度依然由指针B决定，因此高度不变，但因为宽度收缩，所以整体面积会减小。
+    // * 如果下一个高度减小至比指针B高度还小，矩形高度将会由这个更小的高度决定，再加上宽度收缩，因此整体面积同样会减小，而且减小的更多。
+    // 因此我们就有理由不去移动指针A了。我们找到了一种可靠的变化趋势，可以用于精简解空间的搜索，也就不再是穷举法了。
+
+    static int maxContainer2(int[] a) {
+        int left = 0;
+        int right = a.length - 1;
+        int max = Math.max(a[left], a[right]) * a.length;
+        while (left < right) {
+            if (a[left] > a[right]) right--;
+            else left++;
+            max = Math.max(max, Math.max(a[left], a[right]) * (right - left));
+        }
+        return max;
+    }
+
+    /** 解法1：最朴素解法。双指针遍历。Time - o(n^2) */
     static int maxContainer(int[] a) {
         int max = 0;
         for (int i = 0; i < a.length; i++) {
