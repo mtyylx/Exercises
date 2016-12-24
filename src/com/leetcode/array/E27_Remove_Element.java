56fd9c87d3c9559644a1e0a837e8a3fcd23a1b62
Michael Yuan <mtyylx@gmail.com>
2016-12-24 23:59:35 +0800

    Update solutions.

--- src/com/leetcode/array/E27_Remove_Element.java
+++ src/com/leetcode/array/E27_Remove_Element.java
@@ -1,5 +1,7 @@
 package com.leetcode.array;
 
+import java.util.Arrays;
+
 /**
  * Created by LYuan on 2016/8/18.
  * Given an array and a value, remove all instances of that value in place and return the new length.
@@ -21,26 +23,21 @@ package com.leetcode.array;
  */
 public class E27_Remove_Element {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 2, 3, 3};
-        int new_length = removeElement2(nums, 2);
-        System.out.println("new length = " + new_length);
-        for (int i = 0; i < new_length; i++){
-            System.out.print(nums[i] + ",");
-        }
+        int[] a = {1, 2, 2, 3, 3};
+        System.out.println("The New Length is " + removeElement3(a, 2));
+        System.out.println(Arrays.toString(a));
     }
 
+    /** 解法3：双指针同向扫描，逆序扫描，结果乱序。Time - o(n), Space - o(1). */
     // 逆序扫描解法：o(n) 优势是如果要删的元素少，则运算量也小，覆盖次数等效于要删除的元素个数。
     // 发现要删除的元素后，将该元素用数组最后一个元素覆盖，并缩小数组长度。这样可以确保缩小数组长度的同时用有用元素替换无用元素。
-    static int removeElement3(int[] nums, int val) {
-        if (nums == null || nums.length == 0) return 0;
-        int length = nums.length;
-        for (int i = length - 1; i >= 0; i--) {
-            if (nums[i] == val) {
-                nums[i] = nums[length - 1];
-                length--;
-            }
+    static int removeElement3(int[] a, int target) {
+        if (a == null || a.length == 0) return 0;
+        int len = a.length;
+        for (int i = len - 1; i >= 0; i--) {
+            if (a[i] == target) a[i] = a[(len--) - 1];
         }
-        return length;
+        return len;
     }
 
 
