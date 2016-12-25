83a63313e923093db57bb5f62e36a25a710d194d
Michael Yuan <mtyylx@gmail.com>
2016-12-25 12:40:09 +0800

    Add the standard Iterative and Recursive solution of Binary Search and adding code tags.

--- src/com/leetcode/array/E35_Search_Insert_Position.java
+++ src/com/leetcode/array/E35_Search_Insert_Position.java
@@ -5,7 +5,6 @@ package com.leetcode.array;
  * Given a sorted array and a target value, return the index if the target is found.
  * If not, return the index where it would be if it were inserted in order.
  * You may assume no duplicates in the array.
- * 考察Binary Search。
  *
  * Here are few examples.
  * [1,3,5,6], 5 → 2
@@ -15,44 +14,68 @@ package com.leetcode.array;
  *
  * Function Signature:
  * public int insertPos(int[] a, int target) {...}
- * */
+ *
+ * <Tags>
+ * - Binary Search
+ * - Two Pointers: [left → → → ... ← ← ← right]
+ *
+ */
 public class E35_Search_Insert_Position {
     public static void main(String[] args) {
         int[] a = {0, 1, 3, 5, 7, 9};
         System.out.println(insertPos(a, 10));
+        System.out.println(insertPos2(a, 10));
     }
 
-    /** 双指针实现迭代Binary Search: Time - o(logn) */
+    /** 本质是对于二分查找的实现。具体分析可见Basic_Binary_Search.java */
     // 易疏忽点1：求中点的加法有可能会整型溢出，所以最好使用先减后加的方式。
     // 易疏忽点2：while循环条件应包含左右指针重合的情况，只有左右指针位置交错才退出。
     // 易疏忽点3：即使没有找到，target的理想位置也就刚好等于退出while循环后的left指针位置。
-    // Example:
-    // target = -1. [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]     应插入至left所在位置
-    //               ↑     ↑     ↑    ↑  ↑             ↑              ↑ ↑
-    //               l     m     r   l,m r           l,m,r            r l
+    // Example: 证明left刚好处于待插入位置。
+    // 以下四种情况涵盖了left/right指针的所有可能情况，可以看到left最终的位置都能确保是正确的插入位置。
+    // target = 0 [1,  4,  7] -> [1,  4,  7] -> [1,  4,  7] -> [0, 1, 4, 7]
+    //             ↑   ↑   ↑      ↑            ↑ ↑
+    //             l   m   r    l,m,r          r l
     //
-    // target = 5.  [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]     应插入至left所在位置
-    //               ↑     ↑     ↑             ↑  ↑                ↑                ↑ ↑
-    //               l     m     r            l,m r              l,m,r    move l    r l
+    // target = 8 [1,  4,  7] -> [1,  4,  7] -> [1,  4,  7] -> [1, 4, 7, 8]
+    //             ↑   ↑   ↑              ↑              ↑ ↑
+    //             l   m   r            l,m,r            r l
     //
-    // target = 7.  [0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8]     应插入至left所在位置
-    //               ↑     ↑     ↑             ↑  ↑                ↑              ↑ ↑
-    //               l     m     r            l,m r              l,m,r   move r   r l
+    // target = 3 [1,  4,  7] -> [1,  4,  7] -> [1,  4,  7] -> [1, 3, 4, 7]
+    //             ↑   ↑   ↑      ↑              ↑   ↑
+    //             l   m   r    l,m,r            r   l
+    //
+    // target = 6 [1,  4,  7] -> [1,  4,  7] -> [1,  4,  7] -> [1, 4, 6, 7]
+    //             ↑   ↑   ↑              ↑          ↑   ↑
+    //             l   m   r            l,m,r        r   l
+
+    /** 解法2：双指针相向扫描，迭代写法。Time - o(logn) */
     static int insertPos(int[] a, int target) {
-        if (a == null) return 0;
+        if (a == null || a.length == 0) return 0;
         int left = 0;
         int right = a.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (target == a[mid]) return mid;
-            if (target < a[mid]) right = mid - 1;
-            if (target > a[mid]) left = mid + 1;
+            if      (a[mid] > target) right = mid - 1;
+            else if (a[mid] < target) left = mid + 1;
+            else return mid;
         }
         return left;
-//        if (target < a[0]) return 0;
-//        if (target > a[a.length - 1]) return a.length;
-//        if (target < a[left]) return left;
-//        if (target > a[right]) return right;
-//        return -1;
+    }
+
+    /** 解法1：双指针相向扫描，递归写法。Time - o(logn) */
+    static int insertPos2(int[] a, int target) {
+        if (a == null || a.length == 0) return 0;
+        return helper(a, target, 0, a.length - 1);
+    }
+
+    static int helper(int[] a, int target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+            if      (a[mid] < target) return helper(a, target, mid + 1, right);
+            else if (a[mid] > target) return helper(a, target, left, mid - 1);
+            else return mid;
+        }
+        return left;
     }
 }
