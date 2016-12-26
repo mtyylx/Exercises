6b07cfa06659eb9586d3cc9c5d3baac7f2422225
Michael Yuan <mtyylx@gmail.com>
2016-12-26 12:27:20 +0800

    Revise two solutions, add code tags.

--- src/com/leetcode/array/E118_Pascal_Triangle.java
+++ src/com/leetcode/array/E118_Pascal_Triangle.java
@@ -1,7 +1,7 @@
 package com.leetcode.array;
 
-import java.nio.channels.Pipe;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -24,48 +24,67 @@ import java.util.List;
  * 又因为ArrayList<E>实现了List<E>接口，所以最后看到的函数返回类型才是List<List<Integer>>
  *
  * public int[][] pascalTriangle(int numRows) {...}
+ *
+ * <系列问题>
+ * - E118 Pascal Triangle : 给定行号，求解整个杨辉三角。
+ * - E119 Pascal Triangle2: 给定行号，求解杨辉三角的这一行。
+ *
+ * <Tags>
+ * - Min/Max Sentinel
+ * - Reverse Scan
+ *
  */
 public class E118_Pascal_Triangle {
     public static void main(String[] args) {
-        List<List<Integer>> result = pascalTriangle(2000);
-        //print2D(result);
+        print2D(pascalTriangle(20));
+        print2D(pascalTriangle2(20));
     }
 
-    // 改善为从ArrayList尾部追加1在逆序叠加，这样可以避免ArrayList元素频繁右移，大约节省10毫秒。
+    /** 更巧妙地解法：使用同一个List，不断在这个list上修改，并将复制版本加入result。逆序扫描。 */
+    // 计算第i行时，先把第(i-1)行尾部添上一个1，然后逆序相邻叠加。很巧妙。
+    // [1 2 1]
+    // [1 2 1 1]
+    //     \|
+    // [1 2 3 1]
+    //   \|
+    // [1 3 3 1]
     static List<List<Integer>> pascalTriangle2(int nums) {
         List<List<Integer>> result = new ArrayList<>(nums);
-        List<Integer> row = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();              // 无需处理n = 0和n = 1的情况，自动搞定。
         for (int i = 0; i < nums; i++) {
-            row.add(1);
-            for (int j = row.size() - 2; j > 0; j--) {
+            row.add(1);                                     // 在上一行的基础上尾部追加一个1.
+            for (int j = row.size() - 2; j > 0; j--)        // 逆序扫描，否则先改完的会影响后改的元素
                 row.set(j, row.get(j) + row.get(j - 1));
-            }
-            result.add(new ArrayList<>(row));
+            result.add(new ArrayList<>(row));               // 拷贝新实例，切忌直接加入result
         }
         return result;
     }
 
-    // 巧妙在于：没有用传统的“第i+1行的中间元素由第i行斜对角两相邻元素相加而成“规律
-    // 而是使用：每一行元素都等于上一行元素在开头插入元素1后依次叠加而成。
-    // 内循环j从1开始，可以自动包含nums = 1或2不需要叠加的情况。
-    // 缺点是每次都要从头插入元素，需要把所有其他元素都右移一遍。
-    static List<List<Integer>> pascalTriangle(int nums) {
-        List<List<Integer>> result = new ArrayList<>(nums);
-        List<Integer> row = new ArrayList<>();
-        for (int i = 0; i < nums; i++) {
-            row.add(0, 1);
-            for (int j = 1; j < row.size() - 1; j++)
-                row.set(j, row.get(j) + row.get(j + 1));
-            result.add(new ArrayList<>(row));
+    /** 最初解法：每行都由上一行结果相邻叠加而成，使用极大极小值做卫兵避免越界。 */
+    // 针对每一行的元素都是一个倒直角的构造，可以分解为如下面所示：
+    // [1 2 1]   0 1   1 2   2 1   1 0
+    //            \|    \|    \|    \|
+    // [1 3 3 1]   1     3     3     1
+    // 其中首尾的0是虚拟出来的，用卫兵实现。
+    static List<List<Integer>> pascalTriangle(int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (n == 0) return result;
+        if (n > 0) result.add(new ArrayList<>(Arrays.asList(1)));   // 专门处理第0行
+        for (int i = 1; i < n; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                int l = (j > 0) ? result.get(i - 1).get(j - 1) : 0; // 卫兵，虚拟的0
+                int r = (j < i) ? result.get(i - 1).get(j) : 0;     // 卫兵，虚拟的0
+                row.add(l + r);
+            }
+            result.add(row);
         }
         return result;
     }
 
     static void print2D(List<List<Integer>> a) {
         for (List<Integer> row : a) {
-            for (Integer x : row) {
-                System.out.print(x + ",");
-            }
+            for (Integer x : row) System.out.print(x + ",");
             System.out.println("");
         }
     }
