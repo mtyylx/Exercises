5d3edfc32460c765ae11d8b09c17ee8e13b033be
Michael Yuan <mtyylx@gmail.com>
2016-12-31 15:44:06 +0800

    Add question series summary and coding tags.

--- src/com/leetcode/array/E219_Contains_Duplicate_2.java
+++ src/com/leetcode/array/E219_Contains_Duplicate_2.java
@@ -13,34 +13,45 @@ import java.util.HashSet;
  * Function Signature:
  * public boolean containsDuplicate(int[] a, int k) {...}
  *
+ * <系列问题>
+ * E217 Contains Duplicate 1: 给定一个数组，如果相等元素存在就返回true，                           ，否则返回false。
+ * E219 Contains Duplicate 2: 给定一个数组，如果相等元素的索引值之差等于k就返回true                  ，否则返回false。
+ * M220 Contains Duplicate 3: 给定一个数组，如果元素的索引值之差小于等于k且元素值之差小于等于t就返回true，否则返回false。
+ *
+ * <Tags>
+ * - Hash Table: HashSet / HashMap
+ * - Sliding Window
+ *
  */
 public class E219_Contains_Duplicate_2 {
     public static void main(String[] args) {
-        int[] a = {1, 0, 1, 1};
-        System.out.println("Contains Duplicate within K steps: " + containsDuplicate2(a, 1));
+        int[] a = {1, 2, 3, 1};
+        System.out.println("Contains Duplicate within K steps: " + containsDuplicate2(a, 2));
+        System.out.println("Contains Duplicate within K steps: " + containsDuplicate(a, 2));
     }
 
-    // HashSet滑动窗解法，o(n)
-    // 思考角度转变为：只检查当前滑动窗内是否出现重复元素，出现就返回true
+    /** 解法1：HashSet + 滑动窗。Time - o(n), Space - o(n).*/
+    // 滑动窗中的所有元素存在HashSet中，随着滑动窗向右移动，移出的元素需要从HashSet中删掉，移入的被添加，或发现重复。
     static boolean containsDuplicate2(int[] a, int k) {
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < a.length; i++) {
-            if (i > k) set.remove(a[i - k - 1]);    // i > k时窗口开始移动，则从HashSet中删除刚出窗口的元素。
-            if (!set.add(a[i])) return true;        // 在窗口内找到了重复元素，返回true
+            if (!set.add(a[i])) return true;    // 发现重复就返回结果
+            if (i >= k) set.remove(a[i - k]);   // 为下一个循环清除即将被移出滑动窗的元素
         }
         return false;
     }
 
-    // 哈希表解法，o(n)
-    // 关键在于给哈希表的键值对赋予含义：将元素内容存为键，将元素索引存为值
-    // 虽然一个元素可以无限次多的出现在一个数组中，但是我们只关心它的距离是否小于某个值，
-    // 也就是只需要确保重复出现的元素相邻的距离就可以了，例如{9,0,0,9,0,9}中，我们只需关心相邻的9，不需要关系第一个9和最后一个9
+    /** 解法2：HashMap。Time - o(n), Space - o(n). */
+    // Key存数组元素值，Value存数组元素的索引，用于检查是否在滑动窗范围内。
+    // 如果出现重复，那么还需要检查最近的重复和当前位置相距是否超过k
+    // 如果超过k，还要记得更新这个Key的Value，确保存的是最近出现的位置。
+    // 例如 a = [9 0 0 9 0 9], k = 2，第二个9距离第一个9超过了K，但是第三个9距离第二个9等于k。
     static boolean containsDuplicate(int[] a, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < a.length; i++) {
-            if (!map.containsKey(a[i])) map.put(a[i], i);   // Add to HashMap if new
-            else if (i - map.get(a[i]) <= k) return true;   // map.get(a[i])表示最近一次出现时的索引值 只要满足距离小于K就立刻结束搜索
-            else map.put(a[i], i);  // 记得处理这种情况：找到了重复出现的元素，但是距离大于K。必须也要更新哈希表中键对应的值
+            if (!map.containsKey(a[i])) map.put(a[i], i);
+            else if (i - map.get(a[i]) <= k) return true;
+            else map.put(a[i], i);                          // 重复值相距大于k，记得更新索引位置。
         }
         return false;
     }
