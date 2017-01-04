46ee37349e2c630fabecd247731064795b1ed827
Michael Yuan <mtyylx@gmail.com>
2017-01-04 11:09:54 +0800

    Revise solution and add question series summary.

--- src/com/leetcode/array/E278_First_Bad_Version.java
+++ src/com/leetcode/array/E278_First_Bad_Version.java
@@ -12,30 +12,41 @@ package com.leetcode.array;
  *
  * Function Signature:
  * public int firstBadVersion(int n) {...}
+ *
+ * <系列问题>
+ * M35  Search Insert Position: 给定一个已排序数组a和一个目标值k，求k在a中出现的位置值。如果k不在a中，求将k插入a中后让a依然保持有序的位置值。
+ * M34  Search In Range:        给定一个已排序数组a和一个目标值，求k在a中出现的起始和终止位置值。如果k不在a中，则返回[-1, -1]
+ * E278 First Bad Version:      给定一个取值范围1至n（即有序）和一个判定函数，求让判定函数第一次返回true的位置索引。
+ * E374 Guess Number:           给定一个取值范围1至n（即有序）和一个判定函数，求让判定函数返回0的位置索引。
+ *
+ * <Tags>
+ * - Binary Search
+ *
  */
 public class E278_First_Bad_Version {
     public static void main(String[] args) {
         System.out.println(VersionControl.firstBadVersion(9));
+        System.out.println(VersionControl.firstBadVersion2(9));
     }
 }
 
 class VersionControl {
 
-    /** 二分查找解法，迭代写法，区别只是不再是比大小，而是直接判定是或者不是。 */
-    // 需要小心求中点的时候应该用i + (j - i)/2 而不是 (i + j)/2，避免整型溢出。
+    /** 解法1：Binary Search, Iterative. */
+    // 注意1: 求中点时避免整型溢出。
+    // 注意2: 要让左右指针交错再结束循环，此时left指针的位置一定是待插入位置（第一个错误版本）
     static int firstBadVersion(int n) {
-        int left = 0;
-        int right = n - 1;
-        int i;
+        int left = 1;
+        int right = n;
         while (left <= right) {
-            i = left + (right - left) / 2;
-            if (isBadVersion(i)) right = i - 1;
-            else left = i + 1;
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) right = mid - 1;
+            else left = mid + 1;
         }
-        return left;    // 不管什么情况，left指针的位置都刚好是第一个返回true的元素。
+        return left;
     }
 
-    /** 二分查找的递归解法 */
+    /** 解法2：Binary Search, Recursive. */
     static int firstBadVersion2(int n) {
         return divide(1, n);
     }
