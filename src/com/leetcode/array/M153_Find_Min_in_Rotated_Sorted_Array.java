4b982b177424b3b5c5dbb6fcfc55c90ce931799e
Michael Yuan <mtyylx@gmail.com>
2017-01-06 12:49:36 +0800

    Update comments and code tags.

--- src/com/leetcode/array/M153_Find_Min_in_Rotated_Sorted_Array.java
+++ src/com/leetcode/array/M153_Find_Min_in_Rotated_Sorted_Array.java
@@ -8,40 +8,44 @@ package com.leetcode.array;
  * You may assume no duplicate exists in the array.
  *
  * Function Signature:
- * public int findMinRotatedSortedArray(int[] a) {...}
- * */
+ * public int findMin(int[] a) {...}
+ *
+ * <Tags>
+ * - Binary Search
+ * - Two Pointers: 左右指针首尾包围 [left → → → ... ← ← ← right]
+ *
+ */
 public class M153_Find_Min_in_Rotated_Sorted_Array {
     public static void main(String[] args) {
-        int[] a = {3, 1, 2};
-        System.out.println(findMinRotatedSortedArray2(a));
+        System.out.println(findMin2(new int[] {1, 2, 3}));
+        System.out.println(findMin(new int[] {3, 1, 2}));
     }
 
-    // 二分查找解法，o(logn)
-    // 难点在于找到终止条件：即什么情况下就能确认找到了pivot了
-    // 如果中点元素大于右侧指针，则pivot一定在中点元素右边，即i应更新为middle + 1
-    // 如果中点元素不大于右侧指针，则中点本身以及中点左侧的所有元素都有可能是pivot，例如{3, 1, 2}
-    // 直到left和right重合之时，那个元素一定是pivot
-    static int findMinRotatedSortedArray2(int[] a) {
+    /** 解法2：Binary Search的变体。Time - o(logn) */
+    // 与标准的二分查找不同，这里只需要判断a[mid]与a[j]的大小关系就可以移动左右指针了。
+    // 如果a[mid] > a[j]，就说明最小值一定在mid之右，且mid本身不可能是min
+    // 例如[2 3 1]中，a[mid] = 3 > 1，因此最小值只可能在3的右侧
+    // 如果a[mid] <= a[j]，就说明最小值在mid之左，且mid本身可能就是min
+    // 例如[3 1 2]中，a[mid] = 1 < 2，因此最小值在包括1在内的左侧
+    // 试图用标准的i<=j方式解决，但是似乎不如这个来的逻辑清晰
+    static int findMin2(int[] a) {
         if (a == null || a.length == 0) return -1;
-        int left = 0;
-        int right = a.length - 1;
-        while (left < right) {
-            int middle = (left + right) / 2;
-            if (a[middle] > a[right]) left = middle + 1;
-            else  right = middle;
+        int i = 0;
+        int j = a.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (a[mid] > a[j]) i = mid + 1;     // 如果中点mid比右指针j还大，说明最小值范围在[mid + 1, j]，mid本身不可能是最小值
+            else               j = mid;         // 如果中点mid小于等于右指针j，说明最小值范围在[i, mid]，mid本身有可能是最小值
         }
-        return a[left];
+        return a[i];
     }
 
-    // 常规解法，o(n)
-    // 依次扫描，找到比上一个元素小的元素返回。
-    static int findMinRotatedSortedArray(int[] a) {
+    /** 解法1：顺序扫描，最简单。Time - o(n) */
+    // 第一个不满足的元素就是最小值。
+    static int findMin(int[] a) {
         if (a == null || a.length == 0) return -1;
-        int min = a[0];
-        for (int x : a) {
-            if (x < min) return x;
-            else min = x;
-        }
+        for (int i = 1; i < a.length; i++)
+            if (a[i] < a[i - 1]) return a[i];
         return a[0];
     }
 }
