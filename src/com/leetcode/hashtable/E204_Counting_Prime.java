aca9f4a610e28691407372bce17ced6a8fa56e60
Michael Yuan <mtyylx@gmail.com>
2017-01-12 12:40:20 +0800

    Finally get the point of Sieve of Eratosthenes algorithm. Amazing.

--- src/com/leetcode/hashtable/E204_Counting_Prime.java
+++ src/com/leetcode/hashtable/E204_Counting_Prime.java
@@ -9,45 +9,69 @@ import java.util.Set;
  *
  * Function Signature:
  * public int countingPrime(n) {...}
+ *
+ * <Tags>
+ * - HashSet 判重
+ * - Boolean数组作为标志位：读写速度比HashSet更快
+ * - Math: 质数的个数要通过总个数减合数个数得到。
+ *
  */
 public class E204_Counting_Prime {
     public static void main(String[] args) {
-        System.out.println(countPrime(999999));
-        System.out.println(countingPrime2(100));
+        System.out.println(countPrime(9999999));        // 4946 ms
+        System.out.println(countPrime2(9999999));       // 94 ms
+        System.out.println(countPrime3(9999999));       // 62 ms
     }
 
-    // 写法2：扫描至i * i < n，再扫描一遍
-    static int countingPrime2(int n) {
-        // 一开始全认为是Prime
+    /** 解法3：解法2的升级版，两次扫描，内循环指针j的取值范围不断缩小。
+     *  该算法的学名是Sieve of Eratosthenes，是一个很古老的算法，由古希腊亚里士多德之子尼可马库斯记录。*/
+    // i和j不再是乘数1和乘数2。i表示基底，j表示以i为底的结果: i * (i + x) → i*i, i*i + i, i*i + i + i, i*i + i + i + i, ...
+    // i的取值范围：[2, sqrt(n)] 线性扫描，不超过根号n。
+    // j的取值规律：[i^2, i^2 + i, i^2 + 2i, i^2 + 3i, ... n]，多项式扫描，每一项都比前面一项增加i，不超过n。
+    // 由于j的不用每次都从2开始扫描，而是从平方值才开始扫描，因此范围越大，省略的运算越多，效果越好。
+    // 例如n = 100，则i的取值范围是[2, 49]，j的取值范围为[2, 100/i]
+    // 2 → [4, 98] {4,6,8,10,12,14,16,18,20,22,24,26,28,30...
+    // 3 → [9, 99] {9,12,15,18,21,24,27,30...
+    // 4 已有
+    // 5 → [25, 95] {25,30...
+    // ...
+    // 不过相比解法2可以直接一次就统计完毕，这里如果在第一个for循环中统计，将统计不全，必须单开一个重新统计。
+    static int countPrime3(int n) {
+        long start = System.nanoTime();
         boolean[] map = new boolean[n];
+        for (int i = 2; i * i < n; i++) {           // 写成 i * i < n 是因为sqrt运算量较大
+            if (map[i]) continue;                   // 一旦已经判定过是合数就不再继续
+            for (int j = i * i; j < n; j += i)      // j就是
+                map[j] = true;
+        }
         int count = 0;
-        // 从2开始标注，因为0和1都不是质数
-        // 一开始所有的数都是质数，都可以被用来生成自己的合数
-        // 为了避免生成重复的合数，例如2*6=12,3*4=12,4*3=12,6*2=12中前两个没法避免，但后两个可以，只要让i * [i ~ n]范围即可。
-        // 为了进一步避免生成重复的合数，i的范围应该控制在[2 ~ sqrt(n)]之间，因为超过了sqrt(n)的i * j一定会大于n，根本不会有结果。
-        // 又因为sqrt(n)复杂度高，所以写成了i * i < n。
-        for (int i = 2; i * i < n; i++)
-            if (!map[i])
-                for (int j = 2; i * j < n; j++)
-                    map[i * j] = true;
-
-        for (int i = 2; i < n; i++)
-            if (!map[i]) count++;
-
+        for (int i = 2; i < n; i++) if (!map[i]) count++;       // 必须从2开始统计，因为第0和第1个都不算。
+        System.out.println((System.nanoTime() - start)/1000000 + " ms");
         return count;
     }
 
-    // 写法1：扫描一遍，边扫描边计数
-    // 一开始全认为是Prime
-    // 从2开始计算质数，并且同时找出这些质数为底的所有合数，即i*j，j范围则是从i到n，因为从0到i的部分会被前辈搞定。
-    static int countingPrime(int n) {
+    /** 解法2：boolean数组去重（标志位），一次扫描，两个指针的取值范围都从2开始。*/
+    // 对boolean数组的操作要快于对HashSet的插入。解法2是解法1速度的10倍。
+    // 一开始数组所有元素都是false，只要构造出一个合数，就把相应索引的元素置为true。
+    // 同样外循环是乘数1，但扫描范围是[i, n)，由于是顺序扫描，因此只要发现false，就认为这是质数，并且在之后把这个质数对应的所有合数都置为true，
+    // 因此这里count记录的是质数的个数。
+    // 例如n = 100，则i的取值范围是[2, 99]，j的取值范围为[2, 100/i]
+    // 2 * [2, 49] {4,6,8,10,12,14,16,18,20,22,24,26,28,30...
+    // 3 * [2, 33] {6,9,12,15,18,21,24,27,30...
+    // 4 已有
+    // 5 * [2, 19] {10,15,20,25,30...
+    // ...
+    static int countPrime2(int n) {
+        long start = System.nanoTime();
         boolean[] map = new boolean[n];
         int count = 0;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) {           // 2 to n
             if (map[i]) continue;
             count++;
-            for (int j = 2; i * j < n; j++) map[i * j] = true;
+            for (int j = 2; i * j < n; j++)     // 2 to sqrt n
+                map[i * j] = true;
         }
+        System.out.println((System.nanoTime() - start)/1000000 + " ms");
         return count;
     }
 
@@ -60,11 +84,16 @@ public class E204_Counting_Prime {
     // 例如n = 100，则i的取值范围为[2,9]，j的取值范围为[i,100/i]
     // 2 * [2, 49] 共49-2+1=48 {4,6,8,10,12,14,16,18...
     // 3 * [3, 33] 共33-3+1=31 {9,12,15,18,21,24,27...
+    // 4 已有
     // 5 * [5, 19] 共19-5+1=15 {25,30,35...
+    // 6 已有
     // 7 * [7, 14] 共14-7+1=8  {49,56,63...
+    // 8 已有
+    // 9 已有
     // 不过这里计算出的合数依然会出现重复，例如2*6=3*4=12, 2*9=3*6=18
     // 只能利用HashSet来做到自动去重。最后HashSet的大小就是给定范围内的所有合数的个数。剩下的就是质数的个数了。
     static int countPrime(int n) {
+        long start = System.nanoTime();
         if (n < 2) return 0;
         Set<Integer> set = new HashSet<>();
         for (int i = 2; i * i < n; i++) {           // 选择乘数1：从2开始，遍历至根号n
@@ -72,6 +101,7 @@ public class E204_Counting_Prime {
             for (int j = i; i * j < n; j++)         // 选择乘数2：从i开始，扩展到相乘等于n
                 set.add(i * j);
         }
+        System.out.println((System.nanoTime() - start)/1000000 + " ms");
         return n - set.size() - 2;
     }
 }
