c968da215b85fa7eff87085e3b75819bfd06e79f
Michael Yuan <mtyylx@gmail.com>
2017-01-16 13:20:37 +0800

    Revise the solution, keeps getting better.

--- src/com/leetcode/hashtable/E249_Group_Shifted_String.java
+++ src/com/leetcode/hashtable/E249_Group_Shifted_String.java
@@ -4,82 +4,85 @@ import java.util.*;
 
 /**
  * Created by LYuan on 2016/9/1.
- * Given a string, we can "shift" each of its letter to its successive letter,
- * For example: "abc" -> "bcd". We can keep "shifting" which forms the sequence:
- * "abc" -> "bcd" -> ... -> "xyz"
+ *
+ * Given a string, we can "shift" each of its letter to its successive letter, for example: "abc" -> "bcd".
+ * We can keep "shifting" which forms the sequence: "abc" -> "bcd" -> ... -> "xyz"
  * Given a list of strings which contains only lowercase alphabets,
  * group all strings that belong to the same shifting sequence.
  *
- * For example, given: ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"],
+ * For example, given: ["abc", "bcd", "abcd", "xyz", "az", "ba", "a", "z"],
  * Return:
  * [
- *  ["abc","bcd","xyz"],
- *  ["az","ba"],
- *  ["acef"],
- *  ["a","z"]
+ *  ["abc", "bcd", "xyz"],
+ *  ["az", "ba"],
+ *  ["abcd"],
+ *  ["a", "z"]
  * ]
  *
- * Note:
- * For the return value, each inner list's elements must follow the lexicographic order.
- *
  * Function Signature:
- * public List<List<String>> groupString(String[] a) {...}
- * */
+ * public List<List<String>> groupShiftString(String[] a) {...}
+ *
+ * <Tags>
+ * - HashMap: Key → Pattern, Value → BucketID。哈希表用作桶归类的辅助。(与M49所采用的方法完全一致)
+ * - 进制变换：加进制数再求余，可将任何值映射到[0, 进制数 - 1]的区间内。(c[i] - c[0] + N) % N
+ *
+ */
 public class E249_Group_Shifted_String {
     public static void main(String[] args) {
-        String[] a = {"rst", "opq", "abc", "bcd", "acef", "xyz", "az", "ba", "a", "z", "yza"};
-        List<List<String>> result = groupString(a);
+        String[] a = {"rst", "opq", "abc", "bcd", "abcd", "xyz", "az", "ba", "a", "z", "yza"};
+        List<List<String>> result = groupShiftString(a);
     }
 
-    // 哈希表解法，o(n^2)
-    // 难点一：如何定义哈希表的键值含义
-    // 难点二：如何处理a至z的循环偏移关系
-    // 难点三：如何保证每个子list都按字母顺序排序
+    /** 解法1：HashMap桶归类 + 进制变换。Time - o(n). Space - o(c). */
+    /** 关键点1：Hash Map桶归类，与M49思路完全一样。*/
+    // 每一组属于同一模板的字符串都视为一个桶，然后使用HashMap实现桶的分类，模板本身作为Key，桶的编号作为Value。
+    // 需要小心的是，数组引用不能作为HashMap的Key。
+    // 这是因为数组的hashcode并不是通过数组内容计算出来的，而是从数组引用计算得到，因此即使对于两个内容完全相等的数组，其hashcode依然是不同的
+    // 相比之下，String或者List<>都可以做HashMap的Key，因为它们的HashCode都是按照其具体内容计算出来的，因此如果内容相同，就一定指向同一个Key
+    // 这就是为什么这里的getPattern方法最后返回的是String类型。
 
-    // 难点一：哈希表中应该存的键是每一种特殊组合的字符串的模板，以a打头，撑死了有26种变体。
-    // 然后哈希表中应该存的值则是每种独特模板的ArrayList索引值
-    // 判断一个字符串属于那种模板，本质上是比较这个字符串相邻两个元素之间ASCII值之差构成的数组是否与存在哈希表中的模板数组匹配。
-    // 这里有一个坑，就是你不能将数组作为哈希表的键。
-    // 因为你实际存的将会是一个数组的内存指针，而不是数组内容，所以即使两个数组一样，但是由于他们的内存地址不同，因此也无法使用哈希表匹配到。
-    // 所以你必须把这个特征数组转化成为字符串再存成哈希表的键。
-    // 但是还有一个坑，因为数字可以有多位，因此数组转换成为字符串会出现{12, 3} 和 {1, 23}相同的问题
-    // 因此你不能直接把数组转化成为字符串，而是要把数组每个元素变成ASCII字符才行，这样才能确保每个元素都只占一位。
+    /** 关键点2：处理a-to-z的循环关系 - 加进制数后求余。*/
+    // 首先能想到的肯定是求余。但是求余之前需要归一化。
+    // 于是需要把整个字符串都归一化到同一起点再求余：比如全都减掉第一个字符，就可以将任何字符串都归一化为以a打头的pattern
+    // abc  →  0  1  2   (?-a)→   0   1   2   +26→   26 27 28   %26→  0 1 2
+    // yza  →  24 25 0   (?-y)→   0   1 -24   +26→   26 27 2    %26→  0 1 2
+    // zab  →  25 0  1   (?-z)→   0 -25 -24   +26→   26 1  2    %26→  0 1 2
+    // 然后我们就发现，光减掉首字符还不行，因为减完之后可能会出现负值，求余之后值不对。
+    // 这时候我们发现求余运算是周期折叠的，也就是说2%26和(26+2)%26是一样的，因此为了避免负数，可以全都加上进制数26之后，再求余，最后的值一定在[0,25]范围之内
+    // 这样我们就得到了进制变换的公式：(c[i] - c[0] + 26) % 26
+    // 可以推广大到任何进制：(c[i] - c[0] + N) % N
 
-    // 难点二：相邻元素求差值会有正有负，例如'yza'其实和'xyz'一样，但是直接减就变成了[0, 1, -25] vs [0, 1, 1]
-    // 需要使用加26求余的方法，因为最大差值不会超过26，所以加上26可以确保值恒正，再求余就可以回到0~25范围。
-    // 于是就有了下面的代码，可以把任何一个字符串转化成为一个以'a'打头的基础特征。
-    // base[i] = (char)((a.charAt(i) - a.charAt(i - 1) + 26) % 26 + base[i - 1]);
-
-    // 难点三：就是针对List<>接口的内容排序，需要使用Collections类提供的sort，而不是Arrays的。
-    static List<List<String>> groupString(String[] a) {
+    static List<List<String>> groupShiftString(String[] s) {
         List<List<String>> result = new ArrayList<>();
-        Map<String, Integer> pattern = new HashMap<>();
-        int pattern_count = 0;
-        for (String s : a) {
-            String temp = extractPattern(s);
-            if (pattern.containsKey(temp))
-                result.get(pattern.get(temp)).add(s);
-            else {
-                pattern.put(temp, pattern_count);
+        Map<String, Integer> map = new HashMap<>();
+        for (String x : s) {
+            String p = getPattern2(x);
+            if (!map.containsKey(p)) {
                 result.add(new ArrayList<>());
-                result.get(pattern_count).add(s);
-                pattern_count++;
+                map.put(p, result.size() - 1);
             }
+            result.get(map.get(p)).add(x);
         }
-        // Sort each array
-        for (List<String> list : result) {
-            Collections.sort(list);
-        }
-
         return result;
     }
 
-    private static String extractPattern(String a) {
+    /** 新方法：将所有字符都用首字符归一化，简洁清晰。*/
+    // 关键在于 (a.charAt(i) - a.charAt(0) + 26) % 26
+    static String getPattern2(String a) {
+        if (a == null || a.length() == 0) return "";                        // 访问a.charAt(0)之前做好保护工作
+        char[] c = new char[a.length()];
+        for (int i = 0; i < c.length; i++)
+            c[i] = (char) ((a.charAt(i) - a.charAt(0) + 26) % 26 + 'a');    // 加上'a'是为了让得到的pattern也是可读的字符串
+        return new String(c);
+    }
+
+    /** 早期解法：采用<相邻字符依次求差>的方式归一化整个字符串，比较长。*/
+    // 关键在于 (a.charAt(i) - a.charAt(i - 1) + 26) % 26
+    static String getPattern(String a) {
         char[] base = new char[a.length()];
         base[0] = 'a';
-        for (int i = 1; i < base.length; i++) {
+        for (int i = 1; i < base.length; i++)
             base[i] = (char)((a.charAt(i) - a.charAt(i - 1) + 26) % 26 + base[i - 1]);
-        }
         return Arrays.toString(base);
     }
 }
