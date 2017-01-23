17b40551faf0d46bb6e431629fcc791f54d57101
Michael Yuan <mtyylx@gmail.com>
2017-01-23 22:35:32 +0800

    Refine the solutions and add code tags.

--- src/com/leetcode/hashtable/M3_Longest_SubString_Without_Repeating_Characters.java
+++ src/com/leetcode/hashtable/M3_Longest_SubString_Without_Repeating_Characters.java
@@ -15,55 +15,83 @@ import java.util.Map;
  * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  *
  * Function Signature:
- * public int longestSubStringWithoutRepeat(String a) {...}
- * */
+ * public int substring(String a) {...}
+ *
+ * <Tags>
+ * - Two Pointers: 快慢指针同向扫描 [ slow → → → ... fast → → → ... ]
+ * - HashMap: Key → 字符，Value → 出现索引位置
+ * - 字符串扫描：关键是增量扫描，而不是每次都重起炉灶。
+ *
+ */
 public class M3_Longest_SubString_Without_Repeating_Characters {
     public static void main(String[] args) {
-        System.out.println(longestSubStringWithoutRepeat2("abbabbabcdadckdiac"));
+        System.out.println(substring3("abbxyza"));
+        System.out.println(substring2("abbxyza"));
+        System.out.println(substring("abbxyza"));
     }
 
-    // 哈希表+双指针（快慢指针）解法，
-    // 基本思路：
-    // 维护一个哈希表，从第一个元素开始将元素值作为键，元素索引作为值存入哈希表
-    // j为快指针，不断添加或更新现有元素最近一次出现的索引位置
-    // i仅在遇到重复元素时，才会更新为重复的元素最近一次出现的位置的下一个元素，作为重新计算长度的起始位置
-    // 每当快指针移动一次，都重新计算当前已经达到的最大长度
-    static int longestSubStringWithoutRepeat2(String a) {
-        if (a == null || a.length() == 0) return 0;
-        int result = 0;
+    /** 解法1：HashMap + Two Pointers (快慢指针)。Time - o(n), Space - o(n). */
+    // 慢指针指向目标子字符串起始位置，快指针指向子字符串的终止位置，fast - slow + 1就是子字符串的长度。
+    // 指针位置移动的关键逻辑在于：慢指针只有在快指针遇到重复字符且这个字符最近出现位置比慢指针还靠右时才更新，否则只移动慢指针。
+    // 为什么采用这个逻辑：
+    // 示例1："abcbd" slow = 0, fast = 3 时，已经出现了重复元素，因此当前的字符串不再符合要求，
+    // 此时，为了继续向右扫描并寻找符合要求的子字符串，我们只能放弃掉第一个"b"，放弃的方式就是把slow指向第一个"b"的下一个字符，即slow = 2, fast = 3
+    // 同时还要更新最后一次出现"b"的位置，以确保HashMap记录的是字符最近出现的位置
+    // 否则对于"abcbzb"，如果不更新"b"的位置，那么遇到第三个"b"的时候slow跳跃到的位置还是第一个"b"
+    // 示例2："abbxyza"，虽然出现了第二个a，但是由于已经先遇到了第二个b，因此slow的位置已经比第一个a的位置更靠右了，因此无需更新slow的位置。
+    static int substring3(String s) {
+        int slow = 0;
+        int max = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0, j = 0; j < a.length(); j++){
-            if (map.containsKey(a.charAt(j))){
-                // 取max是为了避免abba这种情况，即b重复完了以后又出现了a重复，如果但这时候不能从a的上次出现的接下来位置，而是应该从i的位置开始重新计算
-                i = Math.max(i, map.get(a.charAt(j)) + 1);
+        for (int fast = 0; fast < s.length(); fast++) {
+            char c = s.charAt(fast);
+            if (map.containsKey(c) && map.get(c) + 1 > slow) {
+                slow = map.get(c) + 1;          // 慢指针更新至重复元素的下一个位置开始
+                map.put(c, fast);               // 记得更新每个字符最近的出现位置（而不是第一次出现位置）
+                continue;
             }
-            map.put(a.charAt(j), j);
-            result = Math.max(result, j - i + 1);
+            else map.put(c, fast);
+            max = Math.max(max, fast - slow + 1);
         }
-        return result;
+        return max;
     }
 
+    // 解法1的简化写法
+    static int substring2(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int max = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int slow = 0, fast = 0; fast < s.length(); fast++){
+            if (map.containsKey(s.charAt(fast)))                    // 只要快指针遇到重复元素，就准备更新慢指针
+                slow = Math.max(slow, map.get(s.charAt(fast)) + 1); // 如果慢指针更靠右就不更新，如果重复元素的位置更靠右就更新
+            map.put(s.charAt(fast), fast);                          // 更新元素为最近出现位置
+            max = Math.max(max, fast - slow + 1);                   // 求最大值
+        }
+        return max;
+    }
+
+    // 最初解法，没有利用增量，而是每次都重起炉灶，因此复杂度高。
     // 这个解法思路其实和上面是相似的，但是区别在于遇到重复元素以后怎么处理。
     // 这里选择把整个哈希表都清空，然后指针位置从上次出现重复元素的位置开始，重新扫描添加，所以会效率很低。
-    static int longestSubStringWithoutRepeat(String a) {
-        if (a == null || a.length() == 0) return 0;
+    static int substring(String s) {
+        if (s == null || s.length() == 0) return 0;
         int i = 0;
         int j = i + 1;
         int result = 1;
         Map<Character, Integer> map = new HashMap<>();
-        map.put(a.charAt(0), 0);
-        while (j < a.length()) {
-            if (!map.containsKey(a.charAt(j))) {
-                map.put(a.charAt(j), j);
+        map.put(s.charAt(0), 0);
+        while (j < s.length()) {
+            if (!map.containsKey(s.charAt(j))) {
+                map.put(s.charAt(j), j);
                 j++;
                 result = Math.max(result, j - i);
             }
             else {
                 result = Math.max(result, j - i);
-                i = map.get(a.charAt(j)) + 1;
+                i = map.get(s.charAt(j)) + 1;
                 j = i + 1;
                 map.clear();
-                map.put(a.charAt(i), i);
+                map.put(s.charAt(i), i);
             }
         }
         return result;
