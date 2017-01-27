844a5aec4204138a41467a6790c6e09be0d3c89a
Michael Yuan <mtyylx@gmail.com>
2017-01-28 01:15:53 +0800

    Revise both iterative and recursive solution. Happy Rooster Year!!!

--- src/com/leetcode/linkedlist/E83_Remove_Duplicate_Nodes.java
+++ src/com/leetcode/linkedlist/E83_Remove_Duplicate_Nodes.java
@@ -10,53 +10,68 @@ package com.leetcode.linkedlist;
  *
  * Function Signature:
  * public ListNode removeDuplicate(ListNode head) {...}
- * */
+ *
+ * <Tags>
+ * - Two Pointers：快慢指针同向扫描，快指针始终移动，慢指针仅在值不相同时才移动。[ slow → → → ... fast → → → ... ]
+ * - 递归解法：反向递归，在当前节点和子链表之间做出选择。
+ *
+ */
 public class E83_Remove_Duplicate_Nodes {
     public static void main(String[] args) {
-        ListNode head = ListNode.Generator(new int[] {1, 1, 1, 2, 3, 3, 3, 4, 4, 4});
-        removeDuplicate3(head);
+        removeDuplicate(ListNode.Generator(new int[] {1, 2, 2, 3, 3, 3, 3})).print();
+        removeDuplicate2(ListNode.Generator(new int[] {1, 2, 2, 3, 3, 3, 3})).print();
+        removeDuplicate3(ListNode.Generator(new int[] {1, 2, 2, 3, 3, 3, 3})).print();
     }
 
-    // 递归解法，space - o(n)，有可能overflow
-    // 先递归调用head.next的removeDuplicate，确保返回的节点指针开始后面所有的元素都已没有重复元素
-    // 然后判断head和返回的节点是否重复，如果是，就让head.next指向返回节点的下一个节点（因为这时已经可以保证后面的节点不会重复）
-    // 然后返回head节点（其实只是为了返回整个链表的头指针）
+
+    /** 解法3：递归写法（反向递归）。Time - o(n), Space - o(1). */
+    // 这里head就是当前节点指针（curr）
+    // 首先设置递归终止条件：当前链表为空（实际是原链表的子链表）
+    // 调用递归函数，该函数可以确保返回的子链表中已经没有重复元素，因此head的next一定是指向返回节点的。
+    // 接下来就是判断当前节点和子链表的表头是否相等，如果相等，就应该抛弃当前节点，而直接返回子链表头节点，否则就返回当前节点。
     static ListNode removeDuplicate3(ListNode head) {
-        if (head == null || head.next == null) return head;
-        removeDuplicate3(head.next);
-        if (head.val == head.next.val) head.next = head.next.next;
-        return head;
+        if (head == null) return null;
+        head.next = removeDuplicate3(head.next);
+        if (head.next != null && head.val == head.next.val) return head.next;
+        else return head;
     }
 
-    // 单指针解法，time - o(n), space - o(1)
-    // 判断node.val和node.next.val，如果相同就跳过next节点，如果不同就扫描下一个node
+    /** 解法2：单指针解法。Time - o(n), Space - o(1). */
+    // 很少见单指针的解法比双指针简单。
+    // 不断的检查当前节点与下个节点的值是否相同，如果相同才移动至下个节点，否则跳至下下个节点。
     static ListNode removeDuplicate2(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode node = head;
-        while (node != null) {
-            if (node.next == null) break;
-            if (node.val == node.next.val) node.next = node.next.next;
-            else node = node.next;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) curr.next = curr.next.next;
+            else curr = curr.next;      // 意思就是只有在(curr.next == null || curr.val != curr.next.val)的时候才会直接移动至下一节点
         }
         return head;
     }
 
     /** 解法1：双指针解法（快慢指针）。Time - o(n), Space - o(1). */
     // 快指针负责寻找和慢指针不同的节点，如果相同，就只移动快指针，如果不同，就让慢指针的next指向快指针，并且同时移动快指针和慢指针。
-    // 需要单独处理最后全是相同节点的情况
-    // 需要单独处理空链表的情形
+    // 特例处理1：需要单独处理最后全是相同节点的情况
+    // 1  ->  2 ->  3  ->  null
+    //              ↑      ↑
+    //              slow   fast   正常情况：while循环结束后fast抵达null，slow抵达尾节点。
+    //
+    // 1  ->  2 ->  2  ->  null
+    //        ↑            ↑
+    //        slow         fast   特殊情况：while循环结束后还有重复节点没有处理。
+    //
+    // 特例处理2：需要单独处理空链表的情形
     static ListNode removeDuplicate(ListNode head) {
-        if (head == null) return null;
+        if (head == null) return null;      // 需要单独处理空链表
         ListNode slow = head;
         ListNode fast = head;
         while (fast != null) {
-            if (fast.val != slow.val) {
-                slow.next = fast;
-                slow = slow.next;
+            if (fast.val != slow.val) {     // 只有在快慢指针的节点值不相同时，慢指针才有必要移动
+                slow.next = fast;           // 首先让慢节点跳过所有重复节点，直接与快指针相连
+                slow = slow.next;           // 然后移动慢指针
             }
-            fast = fast.next;
+            fast = fast.next;               // 每一次无论如何都会移动快指针
         }
-        slow.next = null;
+        slow.next = null;                   // 为了处理链表尾端全是相同元素的情况（例如 {1, 2, 2, 2, 2} 最后快指针会抵达null，但慢指针不会）
         return head;
     }
 }
