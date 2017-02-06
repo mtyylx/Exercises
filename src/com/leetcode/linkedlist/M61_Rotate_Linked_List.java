cdf7e946bc3802f0ae83c1f1fc4ca697cbcc9ea2
Michael Yuan <mtyylx@gmail.com>
2017-02-06 12:39:54 +0800

    Update and cleaned up both solutions.

--- src/com/leetcode/linkedlist/M61_Rotate_Linked_List.java
+++ src/com/leetcode/linkedlist/M61_Rotate_Linked_List.java
@@ -9,56 +9,72 @@ package com.leetcode.linkedlist;
  *
  * Function Signature:
  * public ListNode rotateList(ListNode head, int step) {...}
+ *
+ * <Tags>
+ * - <成环再拆环>：链表的独特用法。
+ * - <切断再接续>：链表的传统反折方式。
+ * - Two Pointers: 快慢指针等距扫描。[slow → → → ... fast → → → ... ]
+ *
  */
 public class M61_Rotate_Linked_List {
     public static void main(String[] args) {
-        ListNode head = ListNode.Generator(new int[] {1, 2, 3, 4, 5});
-        ListNode newhead = rotateList2(head, 0);
+        rotateList(ListNode.Generator(new int[] {1, 2, 3, 4, 5}), 2).print();
+        rotateList2(ListNode.Generator(new int[] {1, 2, 3, 4, 5}), 2).print();
     }
 
-    // 双指针解法，找到新位置后直接拼接。
-    static ListNode rotateList2(ListNode head, int step) {
-        if (head == null || head.next == null) return head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode tail = dummy;  // tail
-        ListNode newhead = dummy;  // new head
 
-        int i;
-        for (i = 0; tail.next != null; i++)     //Get the length and tail
-            tail = tail.next;
-
-        for (int j = i - step % i; j > 0; j--)  //Get the newhead
-            newhead = newhead.next;
-
-        tail.next = dummy.next;
-        dummy.next = newhead.next;
-        newhead.next = null;
-
-        return dummy.next;
+    /** 解法2：双指针的创新用法（先成环 再平移 再拆环复原）。Time - o(n), Space - o(1). */
+    // 通常链表的问题都以One-Pass为理想目标，但是这道题由于K可以是任意正整数，因此如果想不检查链表长度与k的关系就平移，会做无用功。
+    // 这里我们还是需要先得到链表长度，然后将链表最后一个节点与头节点相连
+    // 此时再使用双指针平移，最后快慢指针停下来的位置将刚好是新的头节点与尾节点的位置，最后把为尾节点拆掉循环即可。
+    // 例如 k = 2，那么实际移动的次数是 5 - 2 = 3次
+    // 1 -> 2 -> 3 -> 4 -> 5 -> 1 -> 2 -> 3 -> ...
+    // ↑    ↑    ↑    ↑    ↑    ↑    ↑    ↑
+    // s    ↑    ↑    ↑    f    ↑    ↑    ↑             起始位置
+    //      s    ↑    ↑         f    ↑    ↑             第1次
+    //           s    ↑              f    ↑             第2次
+    //                s                   f             第3次
+    //                4 -> 5 -> 1 -> 2 -> 3 -> null    （链表复原）
+    //
+    static ListNode rotateList2(ListNode head, int k) {
+        if (head == null) return null;
+        int count = 1;
+        ListNode curr = head;
+        for (; curr.next != null; count++) curr = curr.next;        // 提前检测终点，为了修改成环
+        if (k % count == 0) return head;                            // Early exit
+        curr.next = head;                                           // 链表成环
+        k = count - k % count;
+        ListNode newHead = head;                                    // 慢指针是newHead，快指针是curr
+        for (int i = 0; i < k; i++) {
+            curr = curr.next;
+            newHead = newHead.next;
+        }
+        curr.next = null;                                           // 链表复原
+        return newHead;
     }
 
-    // 一般链表题目算法都以one pass加不计算长度为荣，但是这道题不一样，因为k可以很大，如果为了one pass不计算长度，那么会反而白白遍历无数次。
-    // 获得链表长度后就知道了平移的距离，这时候把链表首尾相连，平移至新head，再把链表拆开。
-    static ListNode rotateList(ListNode head, int step) {
-        // Get the length first
-        if (head == null) return head;
-        ListNode current = head;
-        ListNode tail;
-        int length = 1;
-        while (current.next != null) {
-            current = current.next;
-            length++;
-        }
-        tail = current;
-        current.next = head;
-        current = head;
-        step = length - step % length;
-        for (int i = 0; i < step; i++) {
-            if (i == step - 1) tail = current;
-            current = current.next;
-        }
-        tail.next = null;
-        return current;
+
+    /** 解法1：传统方法，<链表切断和接续>。Time - o(n), Space - o(1). */
+    // 由于k有可能很大，但是依然是链表长度的整数倍，因此完全无需修改链表，因此首先扫描一遍链表得到长度是很有必要的。
+    //         1 -> 2 -> 3 -> 4 -> 5 -> null       k = 2, 移动步数需要提前停止，因此实际步数应该为 count - k - 1;
+    //           左半部   ↑  |   右半部
+    //                停止位置
+    static ListNode rotateList(ListNode head, int k) {
+        if (head == null) return null;
+        int count = 0;
+        ListNode curr = head;                               // 1st Scan
+        for (; curr != null; count++) curr = curr.next;     // 首先获得链表长度
+        if (k % count == 0) return head;                    // 先判断是否需要继续
+        k = count - k % count - 1;                          // 计算实际移动步数，注意需要减1，因为需要提前停在左半部最后一个节点才能拆分。
+
+        curr = head;                                        // 2nd Scan
+        for (int i = 0; i < k; i++) curr = curr.next;       // 移动至左半部最后一个节点
+        ListNode newHead = curr.next;                       // 缓存新的头节点（右半部第一个节点）
+        curr.next = null;                                   // 清理左半部终点
+
+        curr = newHead;                                     // 3rd Scan
+        while (curr.next != null) curr = curr.next;         // 开始右半部扫描，移动至链表终点
+        curr.next = head;                                   // 重新接合左右两部分
+        return newHead;
     }
 }
