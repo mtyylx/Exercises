80a681f8433b43e0099dec671a2808adb2fce67a
Michael Yuan <mtyylx@gmail.com>
2017-02-07 23:38:01 +0800

    Completely re-written the three insertion solution for linkedlist, gained deeper understanding regarding insertion sort on both arrays and linkedlist.

--- src/com/leetcode/linkedlist/M147_Insertion_Sort_Linked_List.java
+++ src/com/leetcode/linkedlist/M147_Insertion_Sort_Linked_List.java
@@ -1,8 +1,6 @@
 package com.leetcode.linkedlist;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by LYuan on 2016/9/13.
@@ -10,6 +8,14 @@ import java.util.List;
  *
  * Function Signature:
  * public ListNode insertionSort(ListNode head) {...}
+ *
+ * <Tags>
+ * - Linked List to Array: 很多问题并不一定非要限制在一种数据结构中解决，完全可以转化为其他数据结构解决。
+ * - 数组插入排序的两种方式：实际使用双指针实现
+ * - Two Pointers：中心扩散扫描 [ ... ← ← ← sorted | unsorted → → → ... ]
+ *                            [ ... ← ← ← unsorted | sorted → → → ... ]
+ * - 递归：利用逆序递归实现链表的逆序扫描。
+ *
  */
 public class M147_Insertion_Sort_Linked_List {
     public static void main(String[] args) {
@@ -27,34 +33,6 @@ public class M147_Insertion_Sort_Linked_List {
      */
 
 
-    // 迭代解法，time - o(n^2), space - o(1)
-    // 由于传统数组的插入排序是逆序执行的（逆序平移，插入至停止点），因此想要将数组插入排序的算法用在链表上会卡在如何逆序移动的问题上，
-    // 但是只要考虑清楚<链表的优点>在于任何地方插入节点的运算量都是o(1)，完全不用考虑逆序移动的问题，直接插入即可
-    // 因此只要保证顺序扫描的时候保持双指针，检测到待插入节点比prev大比current小就可以插入在current的位置上了
-    // dummy和prev是已排序数组，current是待插入元素
-    // 下面算法设计的一个巧妙的地方在于让dummy和当前链表完全独立开，两者并不连在一起，dummy一开始指向null，然后不断把节点插入dummy链表
-    // 相比于所有节点都在一个链表上，这么做的好处是可以省去判断已排序节点的终点位置
-    static ListNode insertionSortt(ListNode head) {
-        ListNode dummy = new ListNode(0);
-        ListNode prev = dummy;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;   // 缓存current.next节点，因为current有可能被修改引用插入到前面
-            // current与已排序链表（从prev.next开始）一一比较大小，
-            // 如果扫描的的是第一个节点，此时dummy.next = null，已排序链表为null(prev.next = null)，就把current插入dummy与null之间
-            // 如果已排序小，就继续扫描已排序，
-            // 如果已排序大，就把current插入到prev与prev.next之间
-            while (prev.next != null && prev.next.val <= curr.val)
-                prev = prev.next;
-            curr.next = prev.next;   // current后接prev.next
-            prev.next = curr;        // prev后接current
-            prev = dummy;               // 插入完成后重置已排序链表表头
-            curr = next;             // current指针平移
-        }
-        return dummy.next;
-    }
-
-
     /** 解法3：递归解法，基于数组插入排序的解法2（外循环利用逆序递归实现逆序扫描链表，内循环正序扫描） Time - o(n^2), Space - o(n). */
     //  unsorted | sorted
     //   ← 外循环   内循环 →
@@ -63,11 +41,11 @@ public class M147_Insertion_Sort_Linked_List {
     // 难点在于插入节点的条件如何分离：
     // 1. curr.val <= curr.next.val 说明当前节点呆的位置刚好，不需要和curr.next打头的已排序链表交换位置。因此直接返回退出。
     // 2. curr.val > sort.val && sort.next == null 说明curr应该插入到sort的下个节点
-    // 3. curr.val > sort.val && curr.val <= sort.val 说明curr应该插入到sort的下个节点
+    // 3. curr.val > sort.val && curr.val <= sort.val 说明curr应该插入到sort的下个节点（由于链表不能逆向寻址的限制，必须提前判断插入位置，因此必须同时让curr与sort和sort.next比较）
     // 4. curr.val > sort.val && sort.next != null && curr.val > sort.val 说明curr比sort和sort的下个节点都大，应该继续扫描已排序区域进一步确定插入位置。
     // 总结下来，
-    // #1可以提前处理并返回，
-    // #2+#3刚好与#4互斥，因此最后可以一句话做到处理三种情况：
+    // #1可以提前处理并返回，直接用if (curr.val <= curr.next.val) return curr;搞定。
+    // (#2 + #3)整体刚好与#4互斥，因此最后可以一句话做到处理三种情况：
     // while (curr.val > sort.val && sort.next != null && curr.val > sort.next.val) sort = sort.next;
     // 其中，sort.next != null是检查sort.next.val的先觉条件，如果sort.next已经是null了，那么就轮不到检查sort.next.val就可以退出了，避免异常。
     static ListNode insertionSort3(ListNode curr) {
@@ -82,24 +60,33 @@ public class M147_Insertion_Sort_Linked_List {
         return newHead;
     }
 
-    /** 解法2：类似于<链表反转的插入法>。与数组的插入排序思想有一定区别。*/
-    // 显然对于链表来讲，逆序遍历是最需要避免的
+    /** 解法2：迭代写法，基于数组插入排序解法1（但并不完全一样，内循环的扫描方向是与外循环一样的）. Time - o(n^2), Space - o(1).*/
+    // 对于<数组>的插入排序解法1的思路形如：
+    //  sorted | unsorted
+    // ← 内循环   外循环 →
+    // 但对<链表>是不可能实现内循环的逆序扫描的。仔细思考链表和数组的不同点，我们就能发现链表的插入排序是可以让内循环也是正序扫描的。
+    // 因为<数组的元素插入>只能靠平移，而<链表的元素插入>却可以直接在o(1)内完成。
+    //  sorted  |  unsorted
+    //  内循环 →    外循环 →
     // 由于插入的过程头节点有可能会被修改，因此需要使用dummy节点。
+    // 这里巧妙的地方在于并没有一开始就让dummy连上链表头，而是让dummy与当前链表完全独立开。
+    // dummy自成一个已排序的链表，而我们需要做的是不断的把当前链表的节点移植指向dummy打头的链表中的合适位置，直至当前链表清理完毕。
+    // 相比于所有节点都在一个链表上，这么做的好处是可以省去判断已排序节点的终点位置，因为终点位置就是null。
     static ListNode insertionSort2(ListNode head) {
         ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
         ListNode curr = head;
-        while (curr != null) {
-            ListNode sort = dummy;
-            while (sort.next != curr && sort.next.val < curr.val) sort = sort.next;
-            prev.next = curr.next;
-            curr.next = sort.next;
+        while (curr != null) {                                      // 外循环：curr指针正序扫描整个链表
+            ListNode next = curr.next;                              // 缓存curr的下个节点，因为curr即将脱离大部队
+            ListNode sort = dummy;                                  // 内循环：sort指针从dummy开始，同样顺序扫描已排序部分
+            while (sort.next != null && curr.val > sort.next.val)
+                sort = sort.next;
+            curr.next = sort.next;                                  // 找到curr的插入位置，将curr插入sort.next
             sort.next = curr;
-            curr = prev.next;
+            curr = next;                                            // curr移动至原链表下一个节点
         }
         return dummy.next;
     }
+
 
     /** 参考算法：数组插入排序的两种方式 */
     // 解法1：外循环正序遍历数组，已排序区域在左侧，内循环逆序遍历已排序区域
@@ -124,6 +111,7 @@ public class M147_Insertion_Sort_Linked_List {
             a[j - 1] = temp;
         }
     }
+
 
     /** 解法1：转换为数组排序问题。执行速度最快，但是需要额外空间。Time - o(nlogn), Space - o(n). */
     // 说白了，对链表进行排序其实是个很傻的事情，完全可以拷贝到数组里排序，最后再拷贝回来。
