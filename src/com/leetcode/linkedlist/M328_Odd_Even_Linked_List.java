76ac4f26669c260627f37700ae71e3331915330e
Michael Yuan <mtyylx@gmail.com>
2017-02-08 23:25:22 +0800

    Update solution and add code tags.

--- src/com/leetcode/linkedlist/M328_Odd_Even_Linked_List.java
+++ src/com/leetcode/linkedlist/M328_Odd_Even_Linked_List.java
@@ -18,51 +18,62 @@ package com.leetcode.linkedlist;
  * Function Signature:
  * public ListNode splitOddEven(ListNode head) {...}
  *
- * */
+ * <Tags>
+ * - Two Pointers: 同向等速扫描。[slow → → → ... fast → → → ... ]
+ * - Dummy节点：特殊用法，并不在一开始就把Dummy节点于链表头相连，而是不断的把当前链表的节点迁移至Dummy节点打头的新链表上。
+ *
+ */
 public class M328_Odd_Even_Linked_List {
     public static void main(String[] args) {
-        ListNode head = ListNode.Generator(new int[] {1, 2, 3, 4, 5, 6, 7});
-        ListNode newhead = splitOddEven2(head);
+        splitOddEven(ListNode.Generator(new int[] {1, 2, 3})).print();
+        splitOddEven(ListNode.Generator(new int[] {1, 2, 3, 4})).print();
+        splitOddEven2(ListNode.Generator(new int[] {1, 2, 3})).print();
+        splitOddEven2(ListNode.Generator(new int[] {1, 2, 3, 4})).print();
     }
 
-    // 简化写法，因为实际上没有必要分别判断奇偶节点的指针是否为null，偶数总在奇数的右边
-    // even总会在odd之前遇到null，而且不判断even.next.next的好处是even会自动衔接到null上，不需要额外的收尾处理了
+    /** 解法1：双指针（同向等速扫描）。Time - o(n), Space - o(1). */
+    // 需要将任意长度的链表归纳为两种一般的情况：
+    // 奇数长度链表终止情况： ... -> odd -> null          即even == null (或odd.next == null也一样）
+    // 偶数长度链表终止情况： ... -> odd -> even -> null  即even.next == null (或odd.next.next == null也一样）
+    // 循环继续的前提是没有出现上述两种终止情况，因此循环条件应该设置为(even != null && even.next != null)
+    static ListNode splitOddEven(ListNode head) {
+        if (head == null || head.next == null) return head;     // 直接跳过空链表和只有一个节点的链表
+        ListNode odd = head;                                    // 奇指针
+        ListNode even = head.next;                              // 偶指针
+        ListNode evenHead = head.next;                          // 缓存偶链表的头指针
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;                           // 跳过当前even节点
+            odd = odd.next;                                     // 移动至下一个odd节点
+            even.next = even.next.next;                         // 跳过下一个odd节点
+            even = even.next;                                   // 移动至下一个even节点
+        }
+        odd.next = evenHead;                                    // 将奇偶链表相连接
+        return head;
+    }
+
+    /** 解法2：原链表迁移至Dummy节点的解法。Time - o(n), Space - o(1). */
+    // 这里的Dummy节点并没有以上就和链表头相连，而是独立的存在着的
+    // 循环的作用是不断的把奇偶节点从当前链表拆掉，并放置到两个dummy节点引领的奇偶链表上，最后再把两个链表相连即可。
+    // 这种解法思路虽然很独特，但是用在这里并不简洁。
     static ListNode splitOddEven2(ListNode head) {
         if (head == null || head.next == null) return head;
+        ListNode dummy1 = new ListNode(0);      // 只放奇数节点的链表
+        ListNode dummy2 = new ListNode(0);      // 只放偶数节点的链表
+        ListNode curr1 = dummy1;
+        ListNode curr2 = dummy2;
         ListNode odd = head;
         ListNode even = head.next;
-        ListNode connect = head.next;
-        while (even != null && even.next != null) {
-            odd.next = odd.next.next;
-            odd = odd.next;
-            even.next = even.next.next;
-            even = even.next;
+        while (even != null || odd != null) {
+            ListNode nextOdd = (even == null) ? null : even.next;
+            ListNode nextEven = (nextOdd == null) ? null : nextOdd.next;
+            curr1.next = odd;
+            curr1 = curr1.next;
+            curr2.next = even;
+            curr2 = curr2.next;
+            odd = nextOdd;
+            even = nextEven;
         }
-        odd.next = connect;
-        return head;
-    }
-
-    // 典型的双指针解法，time - o(n), space - o(1)
-    // 一个指针指向奇数节点，一个指针指向偶数节点，互为缓存，这样就可以让奇数链接奇数的而不失去偶数的节点引用。
-    static ListNode splitOddEven(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode connect = head.next;
-        while (true) {
-            if (odd.next != null && odd.next.next != null) {
-                odd.next = odd.next.next;
-                odd = odd.next;
-            }
-            else break;
-            if (even.next != null && even.next.next != null) {
-                even.next = even.next.next;
-                even = even.next;
-            }
-            else break;
-        }
-        odd.next = connect;
-        even.next = null;
-        return head;
+        curr1.next = dummy2.next;
+        return dummy1.next;
     }
 }
