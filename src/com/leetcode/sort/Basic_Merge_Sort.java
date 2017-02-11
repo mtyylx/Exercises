dca520c825c3714b4c44b2dd5f5d645ef940744d
Michael Yuan <mtyylx@gmail.com>
2017-02-11 23:54:50 +0800

    Update and comment on the standard merge sort algorithm.

--- src/com/leetcode/sort/Basic_Merge_Sort.java
+++ src/com/leetcode/sort/Basic_Merge_Sort.java
@@ -11,17 +11,24 @@ import java.util.List;
  * Time: o(n * log n) 划分的过程是二分法，因此为logn，合并的过程为n，因此整体时间复杂度是n＊logn.
  * Space: o(n) 因为简单的merge的过程需要临时数组，至于o(1)解法也是可以有的，只不过太过复杂。详细分析见下。
  *
+ * <Tags>
+ * - 二分法：将问题规模分解，使得时间复杂度从 n * n 降至 n * logn.
+ * - 递归：逆序递归
+ * - Sentinel卫兵：同步扫描两个序列时经常用到
+ *
  */
 public class Basic_Merge_Sort {
     public static void main(String[] args) {
-        int[] a = {0, 2, 1, 0};
-        MergeSort_Recursive(a);
-        //MergeSort_Iterative(a);
-        System.out.println(Arrays.toString(a));
-        bulkTest();
+        int[] arr = {9, 8, 7, 5, 2, 4, 1, 6};
+        MergeSort_Iterative(arr);
+        System.out.println(Arrays.toString(arr));
+
+        int[] arr2 = {9, 8, 7, 5, 2, 4, 1, 6};
+        MergeSort_Recursive(arr2);
+        System.out.println(Arrays.toString(arr2));
     }
 
-    /** 迭代解法 - 直接合并 */
+    /** 解法2：分解过程用迭代实现，合并过程同样需要额外空间。Time - o(nlogn), Space - o(n). */
     // Bottom-Up Iterative：相当于省略了递归方法不断除二分解将数组分段的过程，
     // 直接指定最小比较区段就是1，1比完了比2，2比完了比4，一直比到数组长度N
     /** 循环指针含义 */
@@ -49,69 +56,73 @@ public class Basic_Merge_Sort {
     static void MergeSort_Iterative(int[] a) {
         for (int i = 1; i < a.length; i *= 2) {
             for (int j = 0; j < a.length; j += 2 * i) {
-                merge3(a, j, Math.min(j + i - 1, a.length - 1), Math.min(j + 2 * i - 1, a.length - 1));
+                merge(a, j, Math.min(j + i - 1, a.length - 1), Math.min(j + 2 * i - 1, a.length - 1));
                 // System.out.println("Merge(" + j + ", " + Math.min(j + i - 1, a.length - 1) + ", " + Math.min(j + 2 * i - 1, a.length - 1) + ")");
             }
         }
     }
 
-    /** 递归解法 - 先分解再合并 */
+    /** 解法1：<分解过程>用递归实现，<合并过程>需要使用额外空间。Time - o(nlogn), Space - o(n). */
     // Top-down Recursive: 逆序递归，即先递归至终点，再在返回的过程中进行Conquer
+    // 用三层结构实现：
+    // 1. 最外层：Wrapper
+    // 2. 中间层：将数组按二分法进行划分至最小单位（逆序递归中的“先递归”）
+    // 3. 最内层：将划分完的数组区间按顺序进行合并（逆序递归中的“后干活”）
+    // 从整体可以看到实际上这里递归的唯一目的就是对数组区间进行了划分，划分完了之后就可以依次合并排序了。
     // 使用ArrayList实现merge功能，双指针同时扫描两段，并存入ArrayList，最后由ArrayList覆盖当前区间
-    // 用极大值保护先扫描入库完成的那个部分，直至左右两部分都扫完。
+    // 使用卫兵：用极大值保护先扫完的部分，直至左右两部分都扫完。
+    // 左中右left / mid / right三个指针索引都是inclusive的，因此在调用的时候应该调用MergeSort(a, 0, a.length - 1)
     // 算法导论里介绍的方法是先把左右两段元素备份至一个临时数组，然后用双指针扫描更新原数组，
     // 下面的解法则是直接双指针扫描数组两段，存入临时数组，然后顺序扫描更新现有数组，本质上是一样的，不过感觉我的方法写起来更简单些
-    // 需要注意的一点是，这里的left / mid / right索引都是inclusive的，因此在调用的时候应该调用MergeSort(a, 0, a.length - 1)
-    static void MergeSort_Recursive(int[] a) {
-        MergeSortRecursive(a, 0, a.length - 1);
+    static void MergeSort_Recursive(int[] a) {                              // 最外层：Wrapper
+        split(a, 0, a.length - 1);
     }
 
-    static void MergeSortRecursive(int[] a, int left, int right) {
+    static void split(int[] a, int left, int right) {                       // 中间层：二分法划分数组
         if (left < right) {
             int mid = (left + right) / 2;
-            MergeSortRecursive(a, left, mid);
-            MergeSortRecursive(a, mid + 1, right);
-            merge3(a, left, mid, right);
+            split(a, left, mid);
+            split(a, mid + 1, right);
+            merge(a, left, mid, right);
         }
     }
 
-    static void merge(int[] a, int left, int mid, int right) {
+    static void merge(int[] a, int left, int mid, int right) {              // 最内层：对已划分区间进行合并
         List<Integer> list = new ArrayList<>(right - left + 1);
         int i = left;
         int j = mid + 1;
         while (i <= mid || j <= right) {
-            int x, y;
-            if (i <= mid) x = a[i];
-            else x = Integer.MAX_VALUE;
-            if (j <= right) y = a[j];
-            else y = Integer.MAX_VALUE;
+            int x = i <= mid ? a[i] : Integer.MAX_VALUE;                    // 使用卫兵
+            int y = j <= right ? a[j] : Integer.MAX_VALUE;                  // 使用卫兵
             list.add(Math.min(x, y));
-            if (x <= y) i++;
+            if (x < y) i++;
             else j++;
         }
         int idx = left;
-        for (int element : list) {
+        for (int element : list) {                                          // 拷贝回原数组
             a[idx++] = element;
         }
     }
 
-    /** In-place Merge 非常有意思的研究 */
-    // 单独对上面的merge方法进行复杂度分析：Time - o(n + m), Space - o(n + m), 这里n表示左半部长度，m表示右半部长度。
-    // 可以看到，上面的merge方法的时间复杂度已经最优（即左右半部所有元素扫描完就合并完成了，不可能比这个复杂度更低了，因为你必须扫描每个元素，不可能不扫描人家就自动排好。）
-    // 但是，上面merge方法的空间的复杂度是o(n + m)，依然需要额外的空间才能完成，是否可以优化至o(1)即Constant Space呢。这是个非常有意思的问题。
-    // 即问题是：是否可以In-place的进行数组合并呢？
-    // 下面就提供一个In-place的数组合并解法，不过需要注意的是，为了做到In-place Merge，我们不得不提高了时间复杂度至o(n * m)...
-    // 哈哈哈哈...这就是典型的<Time / Space Trade-off>，要不用时间复杂度换空间复杂度，要不然反过来。
+    /** 扩展问题：如何做到o(1)空间复杂度的合并过程？ */
+    // 上面的两种解法又是递归又是迭代，看上去很不同，但是实际上用的是相同的合并算法(merge方法)，而该合并算法的空间复杂度是o(n)。
+    // 那么问题就来了，如何做到o(1)空间复杂度的合并呢？
+    // 可以看到其时间复杂度已经最优，即左右部分的所有元素扫描完就合并完成了，不可能比这个复杂度更低了，因为你必须扫描每个元素，不可能不扫描人家就自动排好。
+    // 但是其空间复杂度是o(n + m)，依然需要额外的空间才能完成，是否可以优化至o(1)即Constant Space呢。这是个非常有意思的问题。
+    // 因此进一步的问题是：是否可以In-place的进行数组合并呢？
+    // 下面就提供一个In-place的数组合并解法，不过需要注意的是，为了做到In-place Merge，我们不得不提高了时间复杂度至o(n * m)
+    // 这就是典型的<Time / Space Trade-off>，要不然用时间复杂度换空间复杂度，要不然用空间换时间，你终归需要付出才有回报。
     // 那么新问题是：是否可以Constant Space且Linear Time的进行数组合并呢？
-    // 其实这个问题到现在还在研究中，可以肯定的是的确存在这种解法，只不过达到这个标准的解法通常都太过复杂，以至于一般人不可能在面试时临时创造出来。
+    // 答案是肯定存在，只不过达到这个标准的解法通常都太过复杂，以至于一般人不可能在面试时临时创造出来。
 
-    /** 上面问题的变体：原位的将两个独立的数组进行整体排序：Time - o(n^2), Space - o(1) */
+    /** 首先解决：对<两个独立数组>进行In-place排序：Time - o(n^2), Space - o(1) */
+    // 其本质是插入排序。
     // 将问题抽象为对两个独立的数组进行merge，使得merge完成后a连接b也是已排序的
     // a = {1, 5, 9, 10, 15, 20}
     // b = {2, 3, 8, 13}
     // 核心思路在于需要在比对两个数组的同时，确保两个数组剩余未扫描部分的元素依然是有序的。
     // 为了确保这一点，需要将两者比对的落选者插入到对方数组的合适部分。所以实际上就是插入排序的过程。
-    static void merge2(int[] a, int[] b) {
+    static void merge_inplace(int[] a, int[] b) {
         int i = a.length - 1;
         int j = b.length - 1;
         while (j >= 0) {
@@ -126,10 +137,10 @@ public class Basic_Merge_Sort {
         }
     }
 
-    /** In-place的Merge方法：对给定区间的数组左右两部分进行Merge，Time - o(n^2), Space - o(1)的Merge方法实现 */
+    /** 然后解决：对<一个数组指定区间内的左右两部分>进行In-place排序：Time - o(n^2), Space - o(1) */
     // 在merge2()方法的基础上修改，得到了可以用与Merge Sort的原位merge方法
     // 需要注意j和x的下限应该相应的改为mid + 1和left。
-    static void merge3(int[] a, int left, int mid, int right) {
+    static void merge_inplace2(int[] a, int left, int mid, int right) {
         int i = mid;
         int j = right;
         while (j >= mid + 1) {
@@ -143,36 +154,4 @@ public class Basic_Merge_Sort {
             j--;
         }
     }
-
-    public static void bulkTest() {
-        for (int j = 2; j < 1000; j += 2) {
-            for (int i = 1; i < 100; i++) {
-                int[] x = randGen(i, j);
-                System.out.println("Original: " + Arrays.toString(x));
-                MergeSort_Recursive(x);
-                if (!isSorted(x)) {
-                    System.out.println("Failed at: " + Arrays.toString(x));
-                    return;
-                }
-                else System.out.println("Passed at: " + Arrays.toString(x));
-            }
-        }
-        System.out.println("Passed.");
-    }
-
-    public static boolean isSorted(int[] a) {
-        if (a == null || a.length < 2) return true;
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] < a[i - 1]) return false;
-        }
-        return true;
-    }
-
-    private static int[] randGen(int len, int range) {
-        int[] a = new int[len];
-        for (int i = 0; i < a.length; i++)
-            a[i] = (int) (range * Math.random());
-        return a;
-    }
-
 }
