613a49e98bba07ccebd3bd22d9ff7619f052992b
Michael Yuan <mtyylx@gmail.com>
2017-02-20 00:09:16 +0800

    Add similar solution to E461.

--- src/com/leetcode/array/E461_Hamming_Distance.java
+++ src/com/leetcode/array/E461_Hamming_Distance.java
@@ -18,22 +18,47 @@ package com.leetcode.array;
  * public int hammingDistance(int x, int y) {...}
  *
  * <Tags>
- * - Bit Manipulation: XOR
- * - Bit Manipulation: Last Digit Extraction
+ * - Bit Manipulation: XOR with ^ operator.
+ * - Bit Manipulation: Bit Mask with & operator.
+ * - Bit Manipulation: x ^ (x - 1) to cleanup the lowest 1 bit.
  *
  */
 public class E461_Hamming_Distance {
     public static void main(String[] args) {
-        System.out.println(hammingDistance(1, 4));      // 0001, 0100
-        System.out.println(hammingDistance(4, 9));      // 0100, 1001
+        System.out.println(hammingDistance1(1, 4));      // 0001, 0100
+        System.out.println(hammingDistance2(4, 9));      // 0100, 1001
+        System.out.println(hammingDistance3(4, 9));      // 0100, 1001
     }
 
-    static int hammingDistance(int x, int y) {
+    /** 解法1：异或 + 与运算Mask + 平移。速度最快。 */
+    static int hammingDistance1(int x, int y) {
         int xor = x ^ y;                // 首先获得两个数值二进制不同位构成的数值
         int count = 0;
         while (xor > 0) {               // 然后提取这个数值中为1的比特个数
             count += xor & 1;           // 通过mask提取最低位数值
             xor >>= 1;                  // 右移直至该值为0
+        }
+        return count;
+    }
+
+    /** 解法2：异或 + 最低位1比特清零法。 */
+    static int hammingDistance2(int x, int y) {
+        int xor = x ^ y;                // 首先获得两个数值二进制不同位构成的数值
+        int count = 0;
+        while (xor > 0) {               // 然后提取这个数值中为1的比特个数
+            xor ^= xor - 1;             // 每异或一次相邻值，xor中最低位的1比特就会被清理为0.
+            count++;
+        }
+        return count;
+    }
+
+    /** 解法3：求余除2法提取比特数。虽然逻辑一样，但是速度明显慢于纯比特运算。 */
+    static int hammingDistance3(int x, int y) {
+        int xor = x ^ y;                // 首先获得两个数值二进制不同位构成的数值
+        int count = 0;
+        while (xor > 0) {               // 然后提取这个数值中为1的比特个数
+            count += xor % 2;           // 求余提取当前最低位比特
+            xor /= 2;                   // 将数值整个向右移动一位（等效于xor >>= 1）
         }
         return count;
     }
