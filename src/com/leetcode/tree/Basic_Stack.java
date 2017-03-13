4ff7889bad485555dc44fa1b92b84bbf6ce8979f
Michael <mtyylx@gmail.com>
2017-03-13 23:45:47 +0800

    Add a blank implementation of Stack. To be filled.

---
+++ src/com/leetcode/tree/Basic_Stack.java
@@ -0,0 +1,16 @@
+package com.leetcode.tree;
+
+/**
+ * Created by Michael on 2017/3/13.
+ * The basic implementation of <Stack> Data Structure.
+ *
+ */
+public class Basic_Stack {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Stack_Array {
+
+}
