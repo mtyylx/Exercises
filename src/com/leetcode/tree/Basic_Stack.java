2a37d9b5d04e547c9662308a3a013a3dad89e1ea
Michael <mtyylx@gmail.com>
2017-03-15 23:58:17 +0800

    Add a array implementation of Stack.

--- src/com/leetcode/tree/Basic_Stack.java
+++ src/com/leetcode/tree/Basic_Stack.java
@@ -7,10 +7,38 @@ package com.leetcode.tree;
  */
 public class Basic_Stack {
     public static void main(String[] args) {
-
+        Stack_Array myStack = new Stack_Array();
+        myStack.add(1);
+        myStack.add(2);
+        myStack.add(3);
+        myStack.add(4);
+        myStack.add(5);
+        myStack.add(6);
     }
 }
 
 class Stack_Array {
+    private int[] stack;
+    private static final int LEN = 5;
+    private int ptr = 0;
 
+    public Stack_Array() {
+        this.stack = new int[LEN];
+    }
+
+    public void add(int x) {
+        if (ptr == stack.length) System.out.println("Stack is Full.");
+        else stack[ptr++] = x;
+    }
+
+    public void remove() {
+        if (ptr == 0) System.out.println("Stack is Empty");
+        else stack[--ptr] = 0;
+    }
+
+    public int peek() {
+        if (ptr != 0) return stack[ptr - 1];
+        System.out.println("Stack is Empty");
+        return -1;
+    }
 }
