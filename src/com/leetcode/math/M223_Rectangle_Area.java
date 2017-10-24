697b0ceed6017cb0f0a73075d39a62edf030f590
Michael <mtyylx@gmail.com>
2017-10-24 20:54:47 +0800

    Add M223. Warming up for a new round.

---
+++ src/com/leetcode/math/M223_Rectangle_Area.java
@@ -0,0 +1,16 @@
+package com.leetcode.math;
+
+/**
+ * Created by Michael on 2017/10/24.
+ * Find the total area covered by two rectilinear rectangles in a 2D plane.
+ * Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+ * Assume that the total area is never beyond the maximum possible value of int.
+ *
+ * <Tags>
+ * - Math: Max of Min, Min of Max
+ * - Bounding Box Calculation in Computer Vision.
+ *
+ */
+
+public class M223_Rectangle_Area {
+}
