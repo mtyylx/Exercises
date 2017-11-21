5d90acd54fad547ed9f7f18cf9be5ea8323cb17f
Michael <mtyylx@gmail.com>
2017-11-21 23:41:15 +0800

    Update M54.

--- src/com/leetcode/array/M54_Spiral_Matrix.java
+++ src/com/leetcode/array/M54_Spiral_Matrix.java
@@ -23,36 +23,51 @@ import java.util.List;
  * - M59 Spiral Matrix 2
  *
  * <Tags>
- * - Array
+ * - Two Pointers: 对向双指针扫描
  *
  */
 public class M54_Spiral_Matrix {
     public static void main(String[] args) {
-
+        int[] a = new int[]{ 1,  2,  3,  4};
+        int[] b = new int[]{10, 11, 12,  5};
+        int[] c = new int[]{ 9,  8,  7,  6};
+        int[][] matrix = new int[][]{a, b, c};
+        System.out.println(spiralOrder(matrix));
+        System.out.println(spiralOrder2(matrix));
     }
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List res = new ArrayList();
+
+    // 双指针扫描
+    //  1  -  2  -  3  -  4
+    //                    |
+    // 10  - 11  - 12     5
+    //  |                 |
+    //  9  -  8  -  7  -  6
+    //
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
         if (matrix.length == 0) return res;
-        int r1 = 0, r2 = matrix.length - 1;
-        int c1 = 0, c2 = matrix[0].length - 1;
-        while (r1 <= r2 && c1 <= c2) {
-            for (int c = c1; c <= c2; c++) res.add(matrix[r1][c]);
-            for (int r = r1 + 1; r <= r2; r++) res.add(matrix[r][c2]);
-            if (r1 < r2 && c1 < c2) {
-                for (int c = c2 - 1; c > c1; c--) res.add(matrix[r2][c]);
-                for (int r = r2; r > r1; r--) res.add(matrix[r][c1]);
+        int row_start = 0;
+        int row_stop = matrix.length - 1;
+        int col_start = 0;
+        int col_stop = matrix[0].length - 1;
+        while (row_start <= row_stop && col_start <= col_stop) {
+            for (int col = col_start; col <= col_stop; col++) res.add(matrix[row_start][col]);         // 上边
+            for (int row = row_start + 1; row <= row_stop; row++) res.add(matrix[row][col_stop]);      // 右边
+            if (row_start < row_stop && col_start < col_stop) {
+                for (int col = col_stop - 1; col > col_start; col--) res.add(matrix[row_stop][col]);   // 下边
+                for (int row = row_stop; row > row_start; row--) res.add(matrix[row][col_start]);      // 左边
             }
-            r1++;
-            r2--;
-            c1++;
-            c2--;
+            row_start++;
+            row_stop--;
+            col_start++;
+            col_stop--;
         }
         return res;
     }
 
-    public List<Integer> spiralOrder2(int[][] matrix) {
-        List res = new ArrayList();
+    public static List<Integer> spiralOrder2(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
         if (matrix.length == 0) return res;
         int R = matrix.length;
         int C = matrix[0].length;
